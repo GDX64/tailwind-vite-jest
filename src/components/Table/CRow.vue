cf1cf0441129517b44c212cc4d9b59067576166f
glmachado <glmachado@nelogica.com.br>
2022-02-06 17:36:25 -0300

    custom table

---
+++ src/components/Table/CRow.vue
@@ -0,0 +1,19 @@
+<template>
+    <tr>
+        <td v-for="col in colConfig">
+            <slot name="cell" :col="col" :row="row">
+                <div class="flex justify-center">{{ row[col.prop] }}</div>
+            </slot>
+        </td>
+    </tr>
+</template>
+
+<script lang="ts" setup>
+export interface ColConfig {
+    prop: string;
+    header: string;
+}
+defineProps<{ row: any, colConfig: ColConfig[] }>()
+
+</script>
+
--- src/pages/Tests.vue
+++ src/pages/Tests.vue
@@ -1,14 +1,29 @@
 <template>
     <BackGroundVue>
-        <BtnTestVue :list="['hello', 'there']"></BtnTestVue>
+        <CTableVue :rows="list" :col-config="colConfig">
+            <template #row="{ row, cols }">
+                <CRow
+                    :row="row"
+                    :col-config="cols"
+                    class="hover:bg-purple-900 even:bg-slate-800"
+                    @click="onClickRow"
+                ></CRow>
+            </template>
+        </CTableVue>
     </BackGroundVue>
 </template>
 
-<script lang="ts" setup>import BtnTestVue from '../components/BtnTest.vue';
+<script lang="ts" setup>
 import BackGroundVue from './BackGround.vue';
+import CTableVue from '../components/Table/CTable.vue';
+import CRow from '../components/Table/CRow.vue';
 
-
-
+const lot = [...Array(10)].map(item => ({ a: 4, b: 5, c: 6 }))
+const list = [{ a: 1, b: 2, c: 3 }, ...lot];
+const colConfig = [{ prop: 'a', header: 'a' }, { prop: 'b', header: 'b' }, { prop: 'c', header: 'c' }]
+function onClickRow(event: MouseEvent) {
+    console.log(event)
+}
 </script>
 
 <style>
