e01f683a6783af2b7ce19e324265bc8718a89223
glmachado <glmachado@nelogica.com.br>
2022-02-06 19:54:29 -0300

    rxjs drag

--- src/pages/Tests.vue
+++ src/pages/Tests.vue
@@ -1,61 +1,107 @@
 <template>
-    <BackGroundVue>
-        <CTableVue :rows="list" :col-config="colConfig">
-            <template #row="{ row, cols }">
-                <CRow
-                    :row="row"
-                    :col-config="cols"
-                    class="hover:bg-purple-900 even:bg-slate-800"
-                    :draggable="true"
-                    :ondragstart="() => dragStart(row)"
-                    :ondragover="(event: DragEvent) => dragOver(event, row)"
-                    :ondrop="(event: DragEvent) => drop(event, row)"
-                ></CRow>
-            </template>
-        </CTableVue>
-    </BackGroundVue>
+  <BackGroundVue>
+    <CTableVue :rows="list" :col-config="colConfig">
+      <template #row="{ row, cols }">
+        <CRow
+          :row="row"
+          :col-config="cols"
+          class="hover:bg-purple-900 even:bg-slate-800"
+          :draggable="true"
+          :ondragstart="() => events.emit('dragStart', row)"
+          :ondragover="(event: DragEvent) => dragOver(event, row)"
+          :ondrop="() => events.emit('drop')"
+          :ondragend="() => events.emit('dragEnd')"
+        ></CRow>
+      </template>
+    </CTableVue>
+  </BackGroundVue>
 </template>
 
 <script lang="ts" setup>
 import BackGroundVue from './BackGround.vue';
 import CTableVue from '../components/Table/CTable.vue';
 import CRow from '../components/Table/CRow.vue';
+import mitt from 'mitt';
+import {
+  fromEventPattern,
+  map,
+  merge,
+  mergeMap,
+  of,
+  switchMap,
+  takeWhile,
+  tap,
+} from 'rxjs';
 import { ref } from 'vue';
 
-const lot = [...Array(10)].map(item => ({ a: 4, b: 5, c: 6 }))
-const list = ref([{ a: 1, b: 2, c: 3 }, ...lot]);
-const colConfig = [{ prop: 'a', header: 'a' }, { prop: 'b', header: 'b' }, { prop: 'c', header: 'c' }]
-const dragged = ref<(typeof lot)[0]>()
-function dragStart(row: any) {
-    console.log(row)
-    dragged.value = row
-}
+const randInt = () => Math.floor(Math.random() * 1000);
+const lot = [...Array(10)].map(() => ({ a: randInt(), b: randInt(), c: randInt() }));
+const list = ref(Object.freeze(lot));
+const colConfig = [
+  { prop: 'a', header: 'a' },
+  { prop: 'b', header: 'b' },
+  { prop: 'c', header: 'c' },
+];
+
+type Row = typeof lot[0];
+type Events = {
+  dragStart: Row;
+  dragOver: Row;
+  dragEnd: void;
+  drop: void;
+};
+const events = mitt<Events>();
+
+const listenTo = <T extends keyof Events>(key: T) =>
+  fromEventPattern<Events[T]>(
+    (handler) => events.on(key, handler),
+    (handler) => events.off(key, handler)
+  );
+
+listenTo('dragStart')
+  .pipe(
+    switchMap((dragged) => {
+      const initialState = [...list.value];
+      const dragOver = listenTo('dragOver').pipe(
+        map((over) => ({ list: swap(dragged, over, list.value), finish: false })),
+        tap(() => console.log('over'))
+      );
+      const drop = listenTo('drop').pipe(
+        mergeMap(() => of({ list: [], finish: true })),
+        tap(() => console.log('drop'))
+      );
+      const mouseUp = listenTo('dragEnd').pipe(
+        mergeMap(() =>
+          of({ list: initialState, finish: false }, { list: [], finish: true })
+        ),
+        tap(() => console.log('end'))
+      );
+      return merge(dragOver, drop, mouseUp).pipe(takeWhile(({ finish }) => !finish));
+    })
+  )
+  .subscribe(({ list: state }) => {
+    list.value = state;
+  });
+
 function dragOver(event: DragEvent, row: any) {
-    event.preventDefault()
-    if (dragged.value) {
-        list.value = swap(row, dragged.value, list.value)
-    }
-
+  event.preventDefault();
+  events.emit('dragOver', row);
 }
-function drop(event: DragEvent, row: any) {
-    event.preventDefault()
-    if (dragged.value) {
-        list.value = swap(row, dragged.value, list.value)
-    }
-}
-function swap<T>(el1: T, el2: T, arr: T[]) {
-    const i1 = list.value.findIndex(el => el === el1)
-    const i2 = list.value.findIndex(el => el === el2)
-    const arrCopy = [...arr]
-    if (i2 === -1 || i1 === -1) {
-        return arrCopy
-    }
-    arrCopy[i2] = el1
-    arrCopy[i1] = el2
-    return arrCopy
 
+function swap<T>(el1: T, el2: T, arr: Readonly<T[]>) {
+  if (el1 === el2) {
+    return arr;
+  }
+  const i1 = list.value.findIndex((el) => el === el1);
+  const i2 = list.value.findIndex((el) => el === el2);
+  if (i2 === -1 || i1 === -1) {
+    return arr;
+  }
+  const arrCopy = [...arr];
+  arrCopy[i2] = el1;
+  arrCopy[i1] = el2;
+  return arrCopy;
 }
 </script>
 
-<style>
-</style>
+<style></style>
