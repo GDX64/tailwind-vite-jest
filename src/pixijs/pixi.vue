3b8e202ee2d9b9786dc36479d134ab501404b860
glmachado <glmachado@nelogica.com.br>
2022-03-09 22:18:41 -0300

    moved chart

--- src/pixijs/pixi.vue
+++ src/pixijs/pixi.vue
@@ -7,7 +7,7 @@ import { onMounted, ref } from 'vue';
 // import { testeo } from '../webgl2/hello';
 // import { main } from '../webgl2/varyings';
 // import { shadersTest } from './shaders';
-import { createTest } from './Chart';
+import { createTest } from './chart/Chart';
 const pixi = ref<HTMLElement>();
 onMounted(() => {
   if (!pixi.value) throw Error('no pixi');
