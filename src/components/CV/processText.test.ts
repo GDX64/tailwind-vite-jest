ff37bb98133f8925e340861b2ac64e191183f8ed
glmachado <glmachado@nelogica.com.br>
2022-03-27 20:58:32 -0300

    process link working

--- src/components/CV/processText.test.ts
+++ src/components/CV/processText.test.ts
@@ -2,7 +2,13 @@ import { processLinks } from './processText';
 
 describe('process links', () => {
   it('should have a link', () => {
-    const result = processLinks('hello there [blabla](google.com) the angel');
-    console.log(result);
+    const result = processLinks('hello [blabla](google.com) angel [hi](hi.com) end');
+    expect(result).toBe(
+      'hello <a href="google.com">blabla</a> angel <a href="hi.com">hi</a> end'
+    );
+  });
+  it('has no link', () => {
+    const result = processLinks('hello there');
+    expect(result).toBe('hello there');
   });
 });
--- src/components/CV/processText.ts
+++ src/components/CV/processText.ts
@@ -1,3 +1,9 @@
 export function processLinks(text: string) {
-  return [...text.matchAll(/\[([^\(\)]*)\]\(([^\(\)]*)\)/g)];
+  return [...text.matchAll(/\[(.*?)\]\((.*?)\)/g)].reduce((acc, match) => {
+    const [textToReplace, label, link] = match;
+    if (textToReplace && label && link) {
+      return acc.replace(textToReplace, `<a href="${link}">${label}</a>`);
+    }
+    return acc;
+  }, text);
 }
