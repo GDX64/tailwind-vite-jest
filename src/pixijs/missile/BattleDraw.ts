add00d1f5a37cc0e5dd10e87d5765ae5de1088a2
glmachado <glmachado@nelogica.com.br>
2022-04-17 19:15:49 -0300

    battleDraw class

--- src/pixijs/missile/BattleDraw.ts
+++ src/pixijs/missile/BattleDraw.ts
@@ -4,42 +4,51 @@ import BattleMap, { Scene } from './BattleMap';
 import Missile from './Missile';
 import { Scale, ScalePair } from '../chart/Scale';
 import { explode } from './Explosion';
-export function test(el: HTMLElement) {
-  const app = new PIXI.Application({
-    backgroundColor: 0xffdddd,
-    height: 900,
-    width: 900,
-  });
 
-  el.appendChild(app.view);
-  const map = new BattleMap(0.05);
-  map.addMissile(createMissile());
-  const g = new Graphics();
-  app.stage.addChild(g);
-  const explosionsContainer = new PIXI.Container();
-  app.stage.addChild(explosionsContainer);
-  app.ticker.add(() => drawScene(map.evolve(), g, app, explosionsContainer));
+export function test(el: HTMLElement) {
+  new BattleDraw(el).animate();
 }
 
-function drawScene(
-  scene: Scene,
-  g: PIXI.Graphics,
-  app: PIXI.Application,
-  explosionsContainer: PIXI.Container
-) {
-  const scaleX = new Scale(0, 100, 0, app.screen.width);
-  const scaleY = new Scale(0, 100, app.screen.height, 0);
-  const scalePair = new ScalePair(scaleX, scaleY);
-  g.clear();
-  scene.missiles.forEach((m) => {
-    g.beginFill(0x000000, 1);
-    const [x, y] = scalePair.scaleVec2(m.pos);
-    g.drawEllipse(x, y, 6, 6);
-    g.endFill();
-  });
-  scene.explosions.forEach((explosion) => {
-    explode(explosionsContainer, scalePair.scaleVec2(explosion.at));
-  });
+class BattleDraw {
+  private map: BattleMap;
+  private g: PIXI.Graphics = new Graphics();
+  private app: PIXI.Application;
+  private explosionsContainer = new PIXI.Container();
+  private scalePair: ScalePair;
+  constructor(el: HTMLElement) {
+    this.app = new PIXI.Application({
+      backgroundColor: 0xffdddd,
+      height: 900,
+      width: 900,
+    });
+    this.scalePair = createScale(this.app.screen.width, this.app.screen.height);
+    el.appendChild(this.app.view);
+    this.map = new BattleMap(0.05);
+    this.map.addMissile(createMissile());
+    this.app.stage.addChild(this.g);
+    this.app.stage.addChild(this.explosionsContainer);
+  }
+
+  animate() {
+    this.app.ticker.add(() => this.drawScene(this.map.evolve()));
+  }
+
+  private drawScene(scene: Scene) {
+    this.g.clear();
+    scene.missiles.forEach((m) => {
+      const [x, y] = this.scalePair.scaleVec2(m.pos);
+      this.g.beginFill(0x000000, 1).drawEllipse(x, y, 6, 6).endFill();
+    });
+    scene.explosions.forEach((explosion) => {
+      explode(this.explosionsContainer, this.scalePair.scaleVec2(explosion.at));
+    });
+  }
+}
+
+function createScale(screenWidth: number, screenHeight: number) {
+  const scaleX = new Scale(0, 100, 0, screenWidth);
+  const scaleY = new Scale(0, 100, screenHeight, 0);
+  return new ScalePair(scaleX, scaleY);
 }
 
 function createMissile() {
