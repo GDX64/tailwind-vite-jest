e2c0816d0c0c25a179f71c8ade19b41f45ccbb89
glmachado <glmachado@nelogica.com.br>
2022-04-24 18:06:32 -0300

    moons promise

--- src/rxjs/PromiseAPI.ts
+++ src/rxjs/PromiseAPI.ts
@@ -1,7 +1,13 @@
-import { getPlanets } from './asyncDemoAPI';
+import * as API from './asyncDemoAPI';
 
 export function getPlanetInfo(planet: string) {
   return new Promise<string[]>((resolve, reject) => {
-    getPlanets(planet, resolve, reject);
+    API.getPlanets(planet, resolve, reject);
+  });
+}
+
+export function getMoonInfo(planet: string) {
+  return new Promise<{ distance: number }>((resolve, reject) => {
+    API.getMoonInfo(planet, resolve, reject);
   });
 }
