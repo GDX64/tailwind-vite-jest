0502d8d82b64c42189d077ac568785ebc01c2f1f
glmachado <glmachado@nelogica.com.br>
2022-04-24 13:13:07 -0300

    improved viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
-import { defineConfig, configDefaults } from 'vitest/config';
+import { configDefaults } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import checker from 'vite-plugin-checker';
 import glsl from 'vite-plugin-glsl';
 import vueJSX from '@vitejs/plugin-vue-jsx';
 import svgLoader from 'vite-svg-loader';
+import { defineConfig } from 'vite';
+import auto from 'autoprefixer';
+import tailwind from 'tailwindcss';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), checker({ typescript: true }), glsl(), vueJSX(), svgLoader()],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    exclude: [...configDefaults.exclude],
-  },
+export default defineConfig((args) => {
+  return {
+    plugins: [vue(), checker({ typescript: true }), glsl(), vueJSX(), svgLoader()],
+    test: {
+      globals: true,
+      environment: 'happy-dom',
+      exclude: [...configDefaults.exclude],
+      include: ['./src/**/**.test.ts', './tests/**.test.ts'],
+    },
+    css: getCssConfig(args.mode),
+  };
 });
+
+function getCssConfig(mode: string) {
+  if (mode === 'test') {
+    return {};
+  }
+  return { postcss: { plugins: [auto(), tailwind()] } };
+}
