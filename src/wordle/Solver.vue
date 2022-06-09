6389904707f8d973a7d2764faf4daf9ba192f8df
glmachado <glmachado@nelogica.com.br>
2022-06-09 19:30:20 -0300

    imporved interactions

--- src/wordle/Solver.vue
+++ src/wordle/Solver.vue
@@ -12,7 +12,11 @@
     />
     <h2>Best guesses</h2>
     <ul>
-      <li v-for="guess of bestGuesses" class="w-40 relative pl-1">
+      <li
+        v-for="guess of bestGuesses"
+        class="w-40 relative pl-1"
+        @click="onWordClick(guess.word)"
+      >
         {{ guess.word }} {{ guess.entropy.toFixed(2) }} bits
         <div
           class="bg-slate-600/20 h-full absolute top-0 left-0 rounded-sm"
@@ -20,11 +24,19 @@
         ></div>
       </li>
     </ul>
+    <h3>Distribution</h3>
+    <div class="flex h-[200px] items-end">
+      <div
+        v-for="value of distribution"
+        class="w-1 bg-slate-500"
+        :style="{ height: `${value * 100}%` }"
+      ></div>
+    </div>
   </BackGround>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import init, { Wordle } from 'wasm';
 import BackGround from '../pages/BackGround.vue';
 
@@ -34,6 +46,12 @@ player.set_ans('hello');
 const inputHistory = ref([] as string[]);
 const input = ref('');
 const bestGuesses = ref(getBestGuesses());
+const distribution = computed(() => {
+  if (isvalidWord(input.value)) {
+    return normalize([...player.distribution_of(input.value)]);
+  }
+  return [];
+});
 
 function onEnter() {
   inputHistory.value = [...inputHistory.value, input.value];
@@ -44,12 +62,23 @@ function onEnter() {
 
 function getBestGuesses() {
   const guesses: [string, number][] = player.calc_best_guesses();
-  const biggestEntropy = guesses.reduce(
-    (acc, [, entropy]) => (acc < entropy ? entropy : acc),
-    0
-  );
+  const biggestEntropy = Math.max(...guesses.map(([, num]) => num));
   return guesses.map(([word, entropy]) => {
     return { word, entropy, percent: entropy / biggestEntropy };
   });
+}
+
+function getDistributionOf(word: string) {}
+
+function normalize(arr: number[]) {
+  const max = Math.max(...arr);
+  return arr.map((value) => value / max);
+}
+
+function onWordClick(word: string) {
+  input.value = word;
+}
+function isvalidWord(word: string) {
+  return word.length === 5;
 }
 </script>
