f826aa3e72285cc093cb16a8289dd6e6c417cb86
glmachado <glmachado@nelogica.com.br>
2022-06-09 20:06:48 -0300

    more interactions

--- src/wordle/Solver.vue
+++ src/wordle/Solver.vue
@@ -1,62 +1,67 @@
 <template>
-  <BackGround>
-    <ul>
-      <li v-for="guess of inputHistory">
-        {{ guess.word }} {{ guess.expected }} {{ guess.information }}
-      </li>
-    </ul>
-    <div class="flex">
-      <input
-        type="text"
-        class="text-black bg-slate-300 rounded-md pl-1"
-        v-model="input"
-        maxlength="5"
-        @keydown.enter="onEnter"
-      />
-      <div>{{ inputEntropy }}</div>
+  <div class="flex">
+    <div class="pr-10">
+      <!-- INPUT CONTAINER -->
+      <div class="flex">
+        <input
+          type="text"
+          class="text-black bg-slate-300 rounded-md pl-1 mr-2"
+          v-model="input"
+          maxlength="5"
+          @keydown.enter="onEnter"
+        />
+        <div>{{ inputEntropy }}</div>
+      </div>
+      <ul>
+        <li v-for="guess of inputHistory">
+          {{ guess.word }} {{ guess.expected }} {{ guess.information }}
+        </li>
+      </ul>
     </div>
-    <h2>Best guesses</h2>
-    <ul>
-      <li
-        v-for="guess of bestGuesses"
-        class="w-40 relative pl-1"
-        @click="onWordClick(guess.word)"
-      >
-        {{ guess.word }} {{ guess.entropy.toFixed(2) }} bits
-        <div
-          class="bg-slate-600/20 h-full absolute top-0 left-0 rounded-sm"
-          :style="{ width: `${guess.percent * 100}%` }"
-        ></div>
-      </li>
-    </ul>
-    <h3>Distribution</h3>
-    <div class="flex h-[200px] items-end">
-      <div
-        v-for="value of distribution"
-        class="w-1 bg-slate-500"
-        :style="{ height: `${value * 100}%` }"
-      ></div>
+    <div>
+      <!-- GUESSES CONTAINER -->
+      <h2>Best guesses</h2>
+      <ul>
+        <li
+          v-for="guess of bestGuesses"
+          class="w-40 relative pl-1"
+          @click="onWordClick(guess.word)"
+        >
+          {{ guess.word }} {{ guess.entropy.toFixed(2) }} bits
+          <div
+            class="bg-slate-600/20 h-full absolute top-0 left-0 rounded-sm"
+            :style="{ width: `${guess.percent * 100}%` }"
+          ></div>
+        </li>
+      </ul>
     </div>
-  </BackGround>
+  </div>
+  <h3>Distribution</h3>
+  <div class="flex h-[200px] items-end">
+    <div
+      v-for="value of distribution"
+      class="bg-slate-500"
+      :style="{ height: `${value * 100}%`, width: '5px', 'margin-right': '1px' }"
+    ></div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { computed, ref } from 'vue';
 import init, { Wordle } from 'wasm';
-import BackGround from '../pages/BackGround.vue';
 
 const {} = await init();
 const player = Wordle.new();
 player.set_ans('hello');
-const inputHistory = ref([] as { word: string; information: number; expected: number }[]);
+const inputHistory = ref([] as { word: string; information: string; expected: string }[]);
 const input = ref('');
 const inputEntropy = computed(() =>
-  isvalidWord(input.value) ? player.entropy_of(input.value).toFixed(2) : 0
+  isvalidWord(input.value) ? player.entropy_of(input.value).toFixed(2) : '0'
 );
 const bestGuesses = ref(getBestGuesses());
 const distribution = computed(() => {
   if (isvalidWord(input.value)) {
-    return normalize([...player.distribution_of(input.value)]);
+    return normalize([...player.distribution_of(input.value)]).sort((a, b) => b - a);
   }
   return [];
 });
@@ -65,7 +70,11 @@ function onEnter() {
   const [, information] = player.play(input.value);
   inputHistory.value = [
     ...inputHistory.value,
-    { word: input.value, information, expected: Number(inputEntropy.value) },
+    {
+      word: input.value,
+      information: information.toFixed(2),
+      expected: inputEntropy.value,
+    },
   ];
   bestGuesses.value = getBestGuesses();
 }
