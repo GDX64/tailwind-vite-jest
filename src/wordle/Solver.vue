6a4ee70b31203bdbc7e8113c3f02c6e18fce47bc
glmachado <glmachado@nelogica.com.br>
2022-06-10 16:15:55 -0300

    better styles

--- src/wordle/Solver.vue
+++ src/wordle/Solver.vue
@@ -1,67 +1,69 @@
 <template>
-  <div class="flex">
-    <div class="pr-10">
-      <!-- INPUT CONTAINER -->
+  <div>
+    <div class="flex">
+      <div class="pr-10">
+        <!-- INPUT CONTAINER -->
+        <div>
+          <div>
+            total information:
+            <span class="text-green-200">{{ totalInformation.toFixed(2) }}</span>
+          </div>
+          <div>
+            you still need:
+            <span class="text-blue-300">{{ neededInformation.toFixed(2) }}</span>
+          </div>
+        </div>
+        <div class="flex items-center">
+          <input
+            type="text"
+            class="my-input mr-2 mt-2 mb-2"
+            v-model="input"
+            maxlength="5"
+            @keydown.enter="onEnter"
+          />
+          <div class="text-yellow-200">{{ inputEntropy }}</div>
+        </div>
+        <ul>
+          <li v-for="guess of inputHistory">
+            <WordleMask :mask="guess.mask" :word="guess.word"></WordleMask>
+            expected: <span class="text-yellow-200">{{ guess.expected }}</span> got:
+            <span class="text-green-200">{{ guess.information }}</span>
+          </li>
+        </ul>
+      </div>
       <div>
-        <div>
-          total information:
-          <span class="text-green-200">{{ totalInformation.toFixed(2) }}</span>
+        <!-- GUESSES CONTAINER -->
+        <div class="flex items-center">
+          <h2 class="pr-3">Best guesses</h2>
+          <Refresh
+            class="w-5 h-5 fill-slate-600 hover:fill-slate-400"
+            @click="onRefresh"
+          ></Refresh>
         </div>
-        <div>
-          you still need:
-          <span class="text-blue-300">{{ neededInformation.toFixed(2) }}</span>
-        </div>
+        <ul>
+          <li
+            v-for="guess of bestGuesses"
+            class="w-40 relative pl-"
+            @click="onWordClick(guess.word)"
+          >
+            {{ guess.word }} {{ guess.entropy.toFixed(2) }} bits
+            <div
+              class="bg-slate-600/20 h-full absolute top-0 left-0 rounded-sm hover:bg-slate-400/20"
+              :style="{ width: `${guess.percent * 100}%` }"
+            ></div>
+          </li>
+        </ul>
       </div>
-      <div class="flex items-center">
-        <input
-          type="text"
-          class="text-black bg-slate-300 rounded-md pl-1 mr-2 mt-2 mb-2"
-          v-model="input"
-          maxlength="5"
-          @keydown.enter="onEnter"
-        />
-        <div class="text-yellow-200">{{ inputEntropy }}</div>
-      </div>
-      <ul>
-        <li v-for="guess of inputHistory">
-          <WordleMask :mask="guess.mask" :word="guess.word"></WordleMask>
-          expected: <span class="text-yellow-200">{{ guess.expected }}</span> got:
-          <span class="text-green-200">{{ guess.information }}</span>
-        </li>
-      </ul>
     </div>
-    <div>
-      <!-- GUESSES CONTAINER -->
-      <div class="flex items-center">
-        <h2 class="pr-3">Best guesses</h2>
-        <Refresh
-          class="w-5 h-5 fill-slate-600 hover:fill-slate-400"
-          @click="onRefresh"
-        ></Refresh>
-      </div>
-      <ul>
-        <li
-          v-for="guess of bestGuesses"
-          class="w-40 relative pl-"
-          @click="onWordClick(guess.word)"
-        >
-          {{ guess.word }} {{ guess.entropy.toFixed(2) }} bits
-          <div
-            class="bg-slate-600/20 h-full absolute top-0 left-0 rounded-sm hover:bg-slate-400/20"
-            :style="{ width: `${guess.percent * 100}%` }"
-          ></div>
-        </li>
-      </ul>
+    <h3>Distribution</h3>
+    <div class="flex h-[200px] items-end">
+      <div
+        v-for="{ value, index } of distribution"
+        @click="onDistributionClick(index)"
+        class="bg-slate-500 hover:bg-slate-400"
+        :style="{ height: `${value * 100}%`, width: '5px', 'margin-right': '1px' }"
+      ></div>
     </div>
-  </div>
-  <h3>Distribution</h3>
-  <div class="flex h-[200px] items-end">
-    <div
-      v-for="{ value, index } of distribution"
-      @click="onDistributionClick(index)"
-      class="bg-slate-500 hover:bg-slate-400"
-      :style="{ height: `${value * 100}%`, width: '5px', 'margin-right': '1px' }"
-    ></div>
   </div>
 </template>
 
@@ -71,22 +73,29 @@ import init, { Wordle } from 'wasm';
 import Refresh from '../assets/arrows-rotate-solid.svg?component';
 import WordleMask from './WordleMask.vue';
 
+const props = defineProps<{ ans: string }>();
 const {} = await init();
+
 const player = Wordle.new();
+player.set_ans(props.ans);
+
 const inputHistory = ref(
   [] as { word: string; information: string; expected: string; mask: number[] }[]
 );
+
 const totalInformation = computed(() => {
   return inputHistory.value.reduce(
     (acc, { information }) => acc + Number(information),
     0
   );
 });
+
 const neededInformation = computed(() => -totalInformation.value - Math.log2(1 / 13_000));
 const input = ref('');
 const inputEntropy = computed(() =>
   isvalidWord(input.value) ? player.entropy_of(input.value).toFixed(2) : '0'
 );
+
 const bestGuesses = ref([] as ReturnType<typeof getBestGuesses>);
 const distribution = computed(() => {
   if (isvalidWord(input.value)) {
