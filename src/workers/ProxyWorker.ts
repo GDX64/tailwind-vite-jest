1603774f25a2fb0fe135f09a24710db7ba358472
glmachado <glmachado@nelogica.com.br>
2022-08-17 21:27:13 -0300

    made expose method

--- src/workers/ProxyWorker.ts
+++ src/workers/ProxyWorker.ts
@@ -1,6 +1,18 @@
-import { defer, filter, finalize, map, Observable, Subject, takeWhile } from 'rxjs';
+import {
+  defer,
+  endWith,
+  filter,
+  finalize,
+  map,
+  mergeMap,
+  Observable,
+  Subject,
+  takeUntil,
+  takeWhile,
+} from 'rxjs';
+import { FinishStream, GenericGet, GenericRequest } from './interfaces';
 
-function makeProxy<T extends {}>(worker: Worker): T {
+export function makeProxy<T extends {}>(worker: Worker): T {
   const get$ = new Subject<GenericGet>();
   worker.addEventListener('message', (message) => {
     if (message.data.type === 'get') {
@@ -8,11 +20,12 @@ function makeProxy<T extends {}>(worker: Worker): T {
     }
   });
   return new Proxy({} as T, {
-    get(target, prop) {
+    get(target, prop: string) {
       return (arg: any, transfer: any) => {
         return defer(() => {
           const id = Math.random();
-          worker.postMessage({ type: 'func', prop, arg, id }, transfer);
+          const genericRequest: GenericRequest = { type: 'func', prop, arg, id };
+          worker.postMessage(genericRequest, transfer);
           return get$.pipe(
             finalize(() => {
               worker.postMessage({ type: 'finish', id });
@@ -27,17 +40,50 @@ function makeProxy<T extends {}>(worker: Worker): T {
   });
 }
 
-interface Talker {
-  hello(x: number): Observable<number>;
+type WorkerMethodsRecord = Record<
+  string,
+  (x: any) => Observable<{ data: any; transfer?: Transferable[] }>
+>;
+
+export function expose(methods: WorkerMethodsRecord) {
+  const func$ = new Subject<GenericRequest>();
+  const finish$ = new Subject<number>();
+  self.addEventListener('message', (message) => {
+    const data = message.data as GenericRequest | FinishStream;
+    if (data.type === 'func') {
+      func$.next(data);
+    } else if (data.type === 'finish') {
+      finish$.next(data.id);
+    }
+  });
+  func$
+    .pipe(mergeMap((data) => makeGenericRequest(data, finish$, methods)))
+    .subscribe((data) => self.postMessage(data, (data.transfer as any) ?? []));
 }
 
-function use() {
-  makeProxy<Talker>({} as any).hello(10);
-}
+function makeGenericRequest(
+  data: GenericRequest,
+  finish$: Observable<number>,
+  methods: WorkerMethodsRecord
+) {
+  const endMessage: GenericGet = {
+    id: data.id,
+    last: true,
+    response: null,
+    type: 'get',
+  };
 
-type GenericGet = {
-  response: any;
-  id: number;
-  type: string;
-  last: boolean;
-};
+  return methods[data.prop](data.arg).pipe(
+    takeUntil(finish$.pipe(filter((id) => id === data.id))),
+    map((answer): GenericGet => {
+      return {
+        id: data.id,
+        last: false,
+        response: answer.data,
+        type: 'get',
+        transfer: answer.transfer,
+      };
+    }),
+    endWith(endMessage)
+  );
+}
---
+++ src/workers/interfaces.ts
@@ -0,0 +1,19 @@
+export type GenericGet = {
+  response: any;
+  id: number;
+  type: 'get';
+  last: boolean;
+  transfer?: Transferable[];
+};
+
+export type GenericRequest = {
+  type: 'func';
+  prop: string;
+  arg: any;
+  id: number;
+};
+
+export type FinishStream = {
+  type: 'finish';
+  id: number;
+};
