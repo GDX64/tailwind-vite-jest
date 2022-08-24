01834695f22a981c480e1a5c190b245043c54da4
glmachado <glmachado@nelogica.com.br>
2022-08-24 09:00:41 -0300

    find path done

--- src/menu/utils.test.ts
+++ src/menu/utils.test.ts
@@ -1,11 +1,17 @@
 import { Item } from './interfaces';
-import { findSelectedPath } from './utils';
+import { findSelected, findSelectedPath } from './utils';
 
 describe('findSelected', () => {
-  it('simple find', () => {
+  it('simple find path', () => {
     const items = [item(), item([item(), item([item([], 'hi')])]), item()];
     const path = findSelectedPath((x) => x.text === 'hi', items);
     expect(path).toEqual([1, 1, 0]);
+  });
+  it('simple find', () => {
+    const items = [item(), item([item(), item([item([], 'hi')])]), item()];
+    const path = findSelectedPath((x) => x.text === 'hi', items);
+    if (!path) throw Error('no path found');
+    expect(findSelected(path, items)?.text).toEqual('hi');
   });
 });
 
--- src/menu/utils.ts
+++ src/menu/utils.ts
@@ -18,3 +18,19 @@ export function findSelectedPath(
   }
   return null;
 }
+
+export function findSelected(path: number[], items: (Item | undefined)[]): Item | null {
+  const [head, ...tail] = path;
+  if (tail.length === 0 && items[head]) {
+    return items[head]!;
+  }
+  if (head != null && items[head]?.items) {
+    return findSelected(tail, items[head]!.items!);
+  }
+  return null;
+}
+
+class MenuTree {
+  path: number[] = [];
+  constructor(private items: Item[]) {}
+}
