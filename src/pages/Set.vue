cc8a38db36dfe3cc0fad661eec7f796912c0ba5c
glmachado <glmachado@nelogica.com.br>
2022-08-27 21:34:13 -0300

    fixed mouse events

--- src/pages/Set.vue
+++ src/pages/Set.vue
@@ -3,11 +3,14 @@
   <div class="absolute text-cyan-200 z-10 top-0 left-0">
     delta {{ regionSeen.x1 - regionSeen.x0 }} | duration: {{ duration }}
   </div>
-  <div class="selected-area absolute bg-slate-400/20" :style="squareStyle"></div>
+  <div
+    class="selected-area absolute bg-slate-300/20 pointer-events-none"
+    :style="squareStyle"
+  ></div>
 </template>
 
 <script setup lang="ts">
-import { endWith, fromEvent, map, switchMap, takeUntil } from 'rxjs';
+import { endWith, filter, fromEvent, map, switchMap, takeUntil } from 'rxjs';
 import { onMounted, ref } from 'vue';
 import { drawSet } from '../mandelbrot/mdSet';
 const canvas = ref<HTMLCanvasElement>();
@@ -27,20 +30,27 @@ const squareStyle = ref({
 });
 onMounted(() => {
   let scales = drawSet(canvas.value!, regionSeen.value);
-  const mouseDown$ = fromEvent<MouseEvent>(canvas.value!, 'mousedown');
+  const mouseDown$ = fromEvent<MouseEvent>(canvas.value!, 'mousedown').pipe(
+    filter((event) => event.button === 0)
+  );
   mouseDown$
     .pipe(
       switchMap((mouseDown) => {
         return fromEvent<MouseEvent>(canvas.value!, 'mousemove').pipe(
           map((move) => {
+            const left = Math.min(mouseDown.clientX, move.clientX);
+            const top = Math.min(mouseDown.clientY, move.clientY);
+            const width = Math.abs(mouseDown.clientX - move.clientX);
+            const height = Math.abs(move.clientY - mouseDown.clientY);
             return {
-              top: toPixel(mouseDown.clientY),
-              left: toPixel(mouseDown.clientX),
-              width: toPixel(Math.abs(mouseDown.clientX - move.clientX)),
-              height: toPixel(Math.abs(move.clientY - mouseDown.clientY)),
+              top: toPixel(top),
+              left: toPixel(left),
+              width: toPixel(width),
+              height: toPixel(height),
               display: 'block',
             };
           }),
+          filter(Boolean),
           takeUntil(fromEvent(window, 'mouseup')),
           endWith({
             top: toPixel(0),
@@ -53,7 +63,6 @@ onMounted(() => {
       })
     )
     .subscribe((region) => {
-      console.log(region);
       squareStyle.value = region;
     });
   mouseDown$
@@ -65,13 +74,17 @@ onMounted(() => {
             const xup = scales.scaleX.transform(mouseUp.clientX);
             const ydown = scales.scaleY.transform(mousedown.clientY);
             const yup = scales.scaleY.transform(mouseUp.clientY);
+            if (Math.abs(mousedown.clientX - mouseUp.clientX) < 5) {
+              return null;
+            }
             return {
               x0: Math.min(xdown, xup),
               x1: Math.max(xdown, xup),
               y0: Math.min(ydown, yup),
               y1: Math.max(ydown, yup),
             };
-          })
+          }),
+          filter(Boolean)
         );
       })
     )
