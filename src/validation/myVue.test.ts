14a4d59b9c4ee2e31a797b2cda727c579d1b8891
glmachado <glmachado@nelogica.com.br>
2022-09-06 22:33:54 -0300

    tested dispose

--- src/validation/myVue.test.ts
+++ src/validation/myVue.test.ts
@@ -1,13 +1,25 @@
 interface MyCTX {
-  awake(): void;
+  awake(): boolean;
+  dispose(): void;
 }
 
 class Computation<T> implements MyCTX {
   dependents = new Set<MyCTX>();
   needsToRun = false;
   value: T;
+  ctx: MyCTX;
   constructor(public calc: (ctx: MyCTX) => T) {
-    this.value = calc(this);
+    this.ctx = this.createNewContext();
+    this.value = calc(this.ctx);
+  }
+
+  private createNewContext(): MyCTX {
+    return {
+      awake: () => this.awake(),
+      dispose() {
+        this.awake = () => false;
+      },
+    };
   }
 
   update(calc: (ctx: MyCTX) => T) {
@@ -22,7 +34,9 @@ class Computation<T> implements MyCTX {
 
   get() {
     if (this.needsToRun) {
-      this.value = this.calc(this);
+      this.ctx.dispose();
+      this.ctx = this.createNewContext();
+      this.value = this.calc(this.ctx);
       this.needsToRun = false;
     }
     return this.value;
@@ -30,8 +44,15 @@ class Computation<T> implements MyCTX {
 
   awake() {
     this.needsToRun = true;
-    this.dependents.forEach((dep) => dep.awake());
+    this.dependents.forEach((dep) => {
+      if (!dep.awake()) {
+        this.dependents.delete(dep);
+      }
+    });
+    return true;
   }
+
+  dispose(): void {}
 }
 
 describe('computation', () => {
@@ -55,5 +76,35 @@ describe('computation', () => {
     expect(comp2.get()).toBe(20);
     comp.update(() => 5);
     expect(comp2.get()).toBe(10);
+  });
+  test('compute dispose', () => {
+    const comp = new Computation(() => 10);
+    const comp2 = new Computation(() => 1);
+    const spy = vitest.fn((ctx: MyCTX) => {
+      if (comp.track(ctx) > 5) {
+        return comp2.track(ctx);
+      }
+      return 0;
+    });
+    const comp3 = new Computation(spy);
+
+    expect(comp3.get()).toBe(1);
+    expect(spy).toHaveBeenCalledTimes(1);
+
+    comp.update(() => 5);
+    expect(comp3.get()).toBe(0);
+    expect(spy).toHaveBeenCalledTimes(2);
+
+    comp2.update(() => 100);
+    expect(comp3.get()).toBe(0);
+    expect(spy).toHaveBeenCalledTimes(2);
+
+    comp.update(() => 6);
+    expect(comp3.get()).toBe(100);
+    expect(spy).toHaveBeenCalledTimes(3);
+
+    comp2.update(() => 11);
+    expect(comp3.get()).toBe(11);
+    expect(spy).toHaveBeenCalledTimes(4);
   });
 });
