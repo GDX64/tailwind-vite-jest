8179d5e455c018fa1333024fc4edf0aab342711f
glmachado <glmachado@nelogica.com.br>
2022-09-08 00:25:17 -0300

    hilbert pure

--- src/pages/Hilbert.vue
+++ src/pages/Hilbert.vue
@@ -14,7 +14,7 @@ function getPairs(n: number, amount: number): Pair[] {
   const v = all_hilbert(2 ** n + 1, amount);
   const arr: Pair[] = Array(v.length / 2);
   for (let i = 0; i < v.length; i += 2) {
-    arr[i / 2] = [v[i] * 10 + 0.5, v[i + 1] * 10 + 0.5];
+    arr[i / 2] = [v[i] * 10, v[i + 1] * 10];
   }
   console.log(arr);
   return arr;
@@ -23,9 +23,13 @@ function getPairs(n: number, amount: number): Pair[] {
 function drawHilbert(pairs: Pair[]) {
   const ctx = canvas.value?.getContext('2d')!;
   ctx.strokeStyle = 'black';
+  ctx.lineWidth = 2;
   ctx.beginPath();
+  ctx.save();
+  ctx.translate(1, 0);
   pairs.forEach(([x, y]) => ctx.lineTo(x, y));
   ctx.stroke();
+  ctx.restore();
 }
 
 onMounted(async () => {
