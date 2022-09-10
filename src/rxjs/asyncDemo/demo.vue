b1c262c08adb8efe3e4f903e55591108d62a5f43
glmachado <glmachado@nelogica.com.br>
2022-09-10 15:19:03 -0300

    building app

--- src/rxjs/asyncDemo/demo.vue
+++ src/rxjs/asyncDemo/demo.vue
@@ -7,18 +7,11 @@
 </template>
 
 <script lang="ts" setup>
-import { onUnmounted, ref, watch } from 'vue';
-import {
-  makeCBRequester,
-  makeObservableRequester,
-  makePromiseRequester,
-} from './callbackRequester';
+import { ref, watch } from 'vue';
+import { makePromiseRequester } from './callbackRequester';
 const planet = ref('');
 const moons = ref([] as { name: string; distance: number }[]);
 const errorMsg = ref('');
-const requester = makeCBRequester(moons, errorMsg);
+const requester = makePromiseRequester(moons, errorMsg);
 watch(planet, (value) => requester.setPlanet(value));
-onUnmounted(() => {
-  requester.unsubscribe();
-});
 </script>
