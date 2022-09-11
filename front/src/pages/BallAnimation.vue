47628a2ecf890e74bdeb77806162f727fd136a28
glmachado <glmachado@nelogica.com.br>
2022-09-11 16:02:47 -0300

    should work on mobile

--- front/src/pages/BallAnimation.vue
+++ front/src/pages/BallAnimation.vue
@@ -1,37 +1,32 @@
 <script setup lang="ts">
 import * as P from 'pixi.js';
-import { onMounted, ref, watchEffect } from 'vue';
-import { animate } from 'popmotion';
-import { zip, fromEvent, Observable, switchMap, animationFrames } from 'rxjs';
-import { Application, Point } from 'pixi.js';
+import { onMounted, onUnmounted, ref } from 'vue';
+import { fromEvent, animationFrames, merge } from 'rxjs';
+import { Application } from 'pixi.js';
 import {
-  RK4Order2,
-  EulerOrder2,
   EulerSystem,
   add,
   scale,
   square,
   normalized,
-  norm,
   V2,
 } from '../unitFormation/UnitFormation';
-import { Chart, registerables } from 'chart.js';
-import { clamp, range, values } from 'ramda';
-Chart.register(...registerables);
+import { clamp, range } from 'ramda';
+
 const pixi = ref<HTMLElement>();
 const canvas = ref<HTMLCanvasElement>();
-const stiffness = ref(250);
-const damping = ref(20);
 const maxClamp = ref(50);
 const much = ref(800);
 const center = ref([400, 500] as V2);
 const centerInfluence = ref(0.5);
+let cleanUp = () => {};
+onUnmounted(() => cleanUp());
 onMounted(() => {
   const app = new P.Application({
     antialias: true,
     backgroundColor: 0xffffff,
-    width: 1000,
-    height: 800,
+    width: window.screen.width,
+    height: window.screen.height,
   });
   pixi.value!.appendChild(app.view);
   const randVar = () => Math.floor(Math.random() * 1000);
@@ -61,55 +56,30 @@ onMounted(() => {
       });
     }
   );
-  const valuesSys = [] as any[];
-  const first = valuesSys.map((v) => ({ x: v.t, y: v.points[0][0], acc: v.acc[0] }));
-  const second = valuesSys.map((v) => ({ x: v.t, y: v.points[1][0] }));
   const balls = sys.points.map(() => new Ball(app));
-  animationFrames().subscribe(() => {
+  const animation = animationFrames().subscribe(() => {
     sys.evolve().points.forEach((point, index) => {
       balls[index].graphics.x = point[0];
       balls[index].graphics.y = point[1];
     });
   });
-  fromEvent<MouseEvent>(app.view, 'mousemove').subscribe((event) => {
-    center.value = [event.offsetX, event.offsetY];
+  const sub = merge(
+    fromEvent<MouseEvent>(app.view, 'mousemove'),
+    fromEvent<TouchEvent>(app.view, 'touchmove')
+  ).subscribe((mouseOrTouch) => {
+    if (mouseOrTouch instanceof TouchEvent) {
+      const touch = mouseOrTouch.touches[0];
+      center.value = [touch.clientX, touch.clientY];
+    } else {
+      center.value = [mouseOrTouch.clientX, mouseOrTouch.clientY];
+    }
   });
+  cleanUp = () => {
+    sub.unsubscribe();
+    animation.unsubscribe();
+    app.destroy(true, { children: true });
+  };
 });
-
-function rk4Chart(canvas: HTMLCanvasElement) {
-  const dt = 1;
-  const N = 500;
-  const rk4 = new RK4Order2(100, 0, dt, 0, (_, x) => -x);
-  const euler = new EulerOrder2(100, 0, dt, 0, (_, x) => -x);
-  const valuesRk4 = range(0, N).map(() => {
-    const { x, t } = rk4.evolve();
-    return { x: t, y: x };
-  });
-  const valuesEuler = range(0, N).map(() => {
-    const { x, t } = euler.evolve();
-    return { x: t, y: x };
-  });
-  const c = new Chart(canvas, {
-    data: {
-      datasets: [
-        {
-          label: 'RK4',
-          borderColor: 'rgb(255, 193, 99)',
-          data: valuesRk4,
-          showLine: true,
-        },
-        {
-          label: 'EULER',
-          borderColor: 'rgb(54, 117, 199)',
-          data: valuesEuler,
-          showLine: true,
-        },
-      ],
-    },
-    type: 'scatter',
-    options: {},
-  });
-}
 
 class Ball {
   graphics;
@@ -118,79 +88,33 @@ class Ball {
     // Rectangle
     graphics.beginFill(0x222222);
     graphics.lineStyle(1, 0xf05555, 1);
-    graphics.drawCircle(0, 0, 10);
+    graphics.drawCircle(0, 0, 8);
     graphics.endFill();
     app.stage.addChild(graphics);
     this.graphics = graphics;
   }
-
-  mouseClick() {
-    fromEvent<MouseEvent>(this.app.view, 'mousedown')
-      .pipe(
-        switchMap((down) => {
-          return zip([
-            springAnimation(this.graphics.x, down.offsetX),
-            springAnimation(this.graphics.y, down.offsetY),
-          ]);
-        })
-      )
-      .subscribe(([x, y]) => {
-        this.graphics.x = x;
-        this.graphics.y = y;
-      });
-  }
-}
-
-type Point = [number, number];
-
-function springAnimation(from: number, to: number) {
-  return new Observable<number>((sub) => {
-    const stop = animate({
-      from,
-      to,
-      stiffness: stiffness.value,
-      damping: damping.value,
-      type: 'spring',
-      onUpdate: (value) => sub.next(value),
-      onComplete: () => sub.complete(),
-    });
-    return () => stop.stop();
-  });
 }
 </script>
 
 <template>
-  <div class="fake-bg fixed w-screen h-screen">
-    <div class="inputs flex">
+  <div class="fixed w-screen h-screen">
+    <div class="inputs flex flex-wrap absolute top-0 left-0">
       <div class="flex">
-        <label for="">stiffness</label>
-        <input type="range" min="0" max="500" step="1" v-model="stiffness" />
-      </div>
-      <div class="flex">
-        <label for="">damping</label>
-        <input type="range" min="0" max="50" step="1" v-model="damping" />
-      </div>
-      <div class="flex">
-        <label for="" class="w-20">max: {{ maxClamp }}</label>
+        <label for="" class="w-36">max repulsion: {{ maxClamp }}</label>
         <input type="range" min="0" max="500" step="1" v-model="maxClamp" />
       </div>
       <div class="flex">
-        <label for="" class="w-20">much: {{ much }}</label>
+        <label for="" class="w-36">repulsion: {{ much }}</label>
         <input type="range" min="0" max="1000" step="1" v-model="much" />
       </div>
       <div class="flex">
-        <label for="" class="w-24">center: {{ centerInfluence }}</label>
+        <label for="" class="w-36">center froce: {{ centerInfluence }}</label>
         <input type="range" min="0" max="3" step="0.01" v-model="centerInfluence" />
       </div>
     </div>
-    <canvas ref="canvas"></canvas>
     <div class="" ref="pixi"></div>
     <slot></slot>
   </div>
 </template>
 
-<style>
-.fake-bg {
-  z-index: -1;
-}
-</style>
+<style></style>
