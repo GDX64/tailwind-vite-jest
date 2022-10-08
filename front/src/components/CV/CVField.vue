409d557425fd71ab5ebc23291c91511761705bdb
glmachado <glmachado@nelogica.com.br>
2022-10-08 11:07:01 -0300

    tippy and styles

--- front/src/components/CV/CVField.vue
+++ front/src/components/CV/CVField.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="hover:bg-sky-100">
+  <div class="">
     <header class="flex justify-between w-full mb-1">
-      <div class="">{{ main }}</div>
+      <TippyWrap>
+        <template #default>
+          <div class="hover:bg-sky-400">{{ main }}</div>
+        </template>
+        <template #content>
+          <div class="bg-sky-800/80 w-96 p-3 rounded-sm">
+            <textarea
+              class="bg-sky-800 w-full text-white rounded-md outline-none"
+              :value="main"
+            />
+            <button
+              class="w-fit h-6 bg-sky-500 text-white rounded-sm pr-2 pl-2 hover:bg-sky-400"
+            >
+              Ok
+            </button>
+          </div>
+        </template>
+      </TippyWrap>
       <div class="">{{ dateAndPlace }}</div>
     </header>
     <div v-if="hasDescription" class="text-gray-700">
@@ -12,6 +29,7 @@
 </template>
 
 <script lang="ts" setup>
+import TippyWrap from '../../TippyWrapper/TippyWrap.vue';
 withDefaults(
   defineProps<{
     main: string;
