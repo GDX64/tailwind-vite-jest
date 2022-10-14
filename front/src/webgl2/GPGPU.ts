317dae99174968877e97ed2a8332e516c09c996f
glmachado <glmachado@nelogica.com.br>
2022-10-14 09:42:36 -0300

    better types

--- front/src/webgl2/GPGPU.ts
+++ front/src/webgl2/GPGPU.ts
@@ -34,7 +34,11 @@ function createShader(gl: WebGL2RenderingContext, type: number, src: string) {
 function makeBuffer(gl: WebGL2RenderingContext, sizeOrData: number | Float32Array) {
   const buf = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buf);
-  gl.bufferData(gl.ARRAY_BUFFER, sizeOrData as any, gl.STATIC_DRAW);
+  if (typeof sizeOrData === 'number') {
+    gl.bufferData(gl.ARRAY_BUFFER, sizeOrData, gl.STATIC_DRAW);
+  } else {
+    gl.bufferData(gl.ARRAY_BUFFER, sizeOrData, gl.STATIC_DRAW);
+  }
   return buf;
 }
 
