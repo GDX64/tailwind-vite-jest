a8e203101e2511a9fb57008b927ae70cf1110f7e
glmachado <glmachado@nelogica.com.br>
2022-10-20 09:29:29 -0300

    twgl cube

--- front/src/pages/GPGPU.vue
+++ front/src/pages/GPGPU.vue
@@ -6,11 +6,13 @@
 import { onMounted, ref } from 'vue';
 import { make } from '../webgl2/GPGPU';
 import { mouseShader } from '../webgl2/shaderToy/MouseShader';
+import { twglTest } from '../webgl2/shaderToy/twglTest';
 const canvas = ref<HTMLCanvasElement>();
 
 onMounted(() => {
   const gl = canvas.value!.getContext('webgl2')!;
-  mouseShader(gl, canvas.value!);
+  // mouseShader(gl, canvas.value!);
+  twglTest(gl, canvas.value!);
   // make(gl!);
 });
 </script>
