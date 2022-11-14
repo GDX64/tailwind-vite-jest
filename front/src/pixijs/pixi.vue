968114b673f22fec4f46dd57503bfa0f714927c4
glmachado <glmachado@nelogica.com.br>
2022-11-14 00:13:28 -0300

    checking tools

--- front/src/pixijs/pixi.vue
+++ front/src/pixijs/pixi.vue
@@ -4,13 +4,14 @@
 
 <script lang="ts" setup>
 import { ref, watchEffect } from 'vue';
+import { graphicsPerf } from './hello/graphicsPerformance';
 import { shadersTest } from './hello/shaders';
 import { shaderTexture } from './hello/shaderTexture';
 import { squaresTest } from './hello/Squares';
 const canvas = ref<HTMLCanvasElement>();
 watchEffect((clear) => {
   if (canvas.value) {
-    clear(shadersTest(canvas.value, devicePixelRatio));
+    clear(graphicsPerf(canvas.value, devicePixelRatio));
   }
 });
 </script>
