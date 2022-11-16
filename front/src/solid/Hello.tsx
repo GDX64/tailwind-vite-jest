b51a1a9698f1d8ae51578eddc332d112c2d2eedb
glmachado <glmachado@nelogica.com.br>
2022-11-15 23:41:07 -0300

    some coll stuff

--- front/src/solid/Hello.tsx
+++ front/src/solid/Hello.tsx
@@ -1,10 +1,11 @@
 import { render, JSX } from '@solidRender/CustomRender';
 import * as PIXI from 'pixi.js';
-import { Accessor, createEffect, createSignal } from 'solid-js';
+import { Accessor, createEffect, createSignal, onCleanup, Show } from 'solid-js';
 
 const CountingComponent = () => {
   const color = createSignal(0xff0000);
-  window.setColor = color[1];
+  const alpha = createSignal(0xff0000);
+  const G = new PIXI.Graphics();
   return (
     <cont>
       <cont x={100} y={100}>
@@ -12,17 +13,38 @@ const CountingComponent = () => {
           <text style={{ fill: color[0]() }} text="hello"></text>
         </cont>
       </cont>
-      <Square color={color[0]()}></Square>
+      {alpha[0]() > 0.5 ? (
+        <Square
+          color={color[0]()}
+          alpha={alpha[0]()}
+          onMouseEnter={() => color[1](0xffff00)}
+          onMouseLeave={() => color[1](0xff0000)}
+        ></Square>
+      ) : (
+        <></>
+      )}
     </cont>
   );
 };
 
-function Square(arg: { color: number }) {
+function Square(arg: {
+  color: number;
+  alpha: number;
+  onMouseEnter: () => void;
+  onMouseLeave: () => void;
+}) {
   const g = new PIXI.Graphics();
   createEffect(() => {
     g.clear();
     g.beginFill(arg.color).drawRect(0, 0, 100, 100).endFill();
   });
+  createEffect(() => {
+    g.alpha = arg.alpha;
+  });
+  g.interactive = true;
+  g.addListener('mouseenter', arg.onMouseEnter);
+  g.addListener('mouseleave', arg.onMouseLeave);
+  onCleanup(() => g.destroy());
   return g;
 }
 
