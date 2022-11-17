1ecf7fd8d8c0d4e193666d5aca226ab2bdf9997e
glmachado <glmachado@nelogica.com.br>
2022-11-16 23:38:07 -0300

    actions

--- front/src/pixijs/domLike/domLike.tsx
+++ front/src/pixijs/domLike/domLike.tsx
@@ -5,16 +5,20 @@ import { range } from 'ramda';
 import { Accessor, createEffect, createSignal, onCleanup, Signal } from 'solid-js';
 
 interface TableData {
-  values: { text: Signal<string> }[];
-  height: number;
+  values: Signal<{ text: Signal<string>; birth: Signal<Date> }[]>;
+  height: Signal<number>;
 }
 
 function dataGen(): TableData {
   const cats = range(0, 10).map(() => {
-    const text = faker.animal.cat();
-    return { text: createSignal(text) };
+    return randomCat();
   });
-  return { height: 20, values: cats };
+  return { height: createSignal(20), values: createSignal(cats) };
+}
+
+function randomCat() {
+  const text = faker.animal.cat();
+  return { text: createSignal(text), birth: createSignal(faker.date.birthdate()) };
 }
 
 export function setupDomTest(view: HTMLCanvasElement, resolution: number) {
@@ -33,24 +37,69 @@ function CreateTable(args: { data: Accessor<TableData> }) {
   createBitMapFonts(devicePixelRatio);
   return (
     <cont x={100} y={100} cacheAsBitmap={false}>
-      <For each={args.data().values}>
-        {(item, index) => <Row text={item.text} y={index() * args.data().height}></Row>}
-      </For>
+      <Btn
+        value="height +"
+        onClick={() =>
+          args.data().height[1]((other) => {
+            console.log('click');
+            return other + 1;
+          })
+        }
+        y={0}
+      ></Btn>
+      <Btn
+        value="cat +"
+        onClick={() =>
+          args.data().values[1]((other) => {
+            console.log('click');
+            return [randomCat(), ...other];
+          })
+        }
+        y={20}
+      ></Btn>
+      <cont y={40}>
+        <For each={args.data().values[0]()}>
+          {(item, index) => (
+            <Row
+              text={item.text}
+              y={index() * args.data().height[0]()}
+              birth={item.birth}
+            ></Row>
+          )}
+        </For>
+      </cont>
     </cont>
   );
 }
 
-function Row(arg: { text: Signal<string>; y: number }) {
-  const tx = new PIXI.BitmapText(arg.text[0](), { fontName: 'black' });
+function Btn(props: { value: string; onClick: () => void; y: number }) {
+  const txt = new PIXI.Text(props.value);
+  txt.style.fontSize = 12;
+  txt.interactive = true;
+  txt.addListener('click', props.onClick);
+  txt.y = props.y;
+  return txt;
+}
+
+function Row(args: { text: Signal<string>; birth: Signal<Date>; y: number }) {
+  return (
+    <cont y={args.y}>
+      <NameCell text={args.text[0]()} x={0}></NameCell>
+      <NameCell text={args.birth[0]().toUTCString()} x={100}></NameCell>
+    </cont>
+  );
+}
+
+function NameCell(args: { text: string; x: number }) {
+  const tx = new PIXI.BitmapText(args.text, { fontName: 'black' });
   tx.interactive = true;
   tx.addListener('mouseenter', () => (tx.alpha = 0.5));
   tx.addListener('mouseleave', () => (tx.alpha = 1));
-  tx.addListener('click', () => arg.text[1](faker.animal.cat()));
   console.log('montei');
   createEffect(() => {
     console.log('update');
-    tx.text = arg.text[0]();
-    tx.y = arg.y;
+    tx.text = args.text;
+    tx.x = args.x;
   });
   onCleanup(() => console.log('clean'));
   return tx;
