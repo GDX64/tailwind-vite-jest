6013566bc8c66d54321ef3658d8af7ada66edd67
glmachado <glmachado@nelogica.com.br>
2022-11-19 21:39:40 -0300

    header

--- front/src/pixijs/domLike/domLike.tsx
+++ front/src/pixijs/domLike/domLike.tsx
@@ -3,7 +3,7 @@ import * as PIXI from 'pixi.js';
 import { range } from 'ramda';
 import { animationFrames } from 'rxjs';
 import { render, For } from '@solidRender/CustomRender';
-import { createMemo } from 'solid-js';
+import { createMemo, createSignal } from 'solid-js';
 import { createMutable } from 'solid-js/store';
 import { NameCell, Btn, Graphic } from './Components';
 
@@ -55,6 +55,12 @@ function calcAge(date: Date) {
 
 function CreateTable() {
   createBitMapFonts(devicePixelRatio);
+  const [columnsSize] = createSignal([0, 150, 280, 320] as [
+    number,
+    number,
+    number,
+    number
+  ]);
   const oldest = createMemo(() => {
     const oldest = store.values.reduce((a, b) => (a < b.birth ? a : b.birth), new Date());
     return calcAge(oldest);
@@ -89,7 +95,10 @@ function CreateTable() {
         }}
         p_y={20}
       ></Btn>
-      <cont y={40}>
+      <cont y={55}>
+        <Header pos={columnsSize()}></Header>
+      </cont>
+      <cont y={80}>
         <For each={sortedByAge()}>
           {(item, index) => (
             <Row
@@ -98,10 +107,22 @@ function CreateTable() {
               birth={item.birth}
               oldest={oldest()}
               median={median()}
+              positions={columnsSize()}
             ></Row>
           )}
         </For>
       </cont>
+    </cont>
+  );
+}
+
+function Header(args: { pos: [number, number, number, number] }) {
+  return (
+    <cont>
+      <NameCell p_x={args.pos[0]} p_text="Cat breed" p_fontName="bold-black"></NameCell>
+      <NameCell p_x={args.pos[1]} p_text="Birth" p_fontName="bold-black"></NameCell>
+      <NameCell p_x={args.pos[2]} p_text="Age" p_fontName="bold-black"></NameCell>
+      <NameCell p_x={args.pos[3]} p_text="Proportion" p_fontName="bold-black"></NameCell>
     </cont>
   );
 }
@@ -112,19 +133,20 @@ function Row(args: {
   oldest: number;
   y: number;
   median: Date;
+  positions: [number, number, number, number];
 }) {
   const age = createMemo(() => calcAge(args.birth));
   const proportion = createMemo(() => (100 * age()) / args.oldest);
   return (
     <cont y={args.y}>
-      <NameCell p_text={args.text} p_x={0}></NameCell>
-      <NameCell p_text={args.birth.toDateString()} p_x={150}></NameCell>
+      <NameCell p_text={args.text} p_x={args.positions[0]}></NameCell>
+      <NameCell p_text={args.birth.toDateString()} p_x={args.positions[1]}></NameCell>
       <NameCell
         p_text={String(age())}
-        p_x={300}
+        p_x={args.positions[2]}
         p_fontName={args.birth < args.median ? 'red' : 'black'}
       ></NameCell>
-      <Graphic x={350} color={Math.round((1 - proportion() / 100) * 255)}>
+      <Graphic x={args.positions[3]} color={Math.round((1 - proportion() / 100) * 255)}>
         {[new PIXI.Rectangle(0, 0, proportion(), 10)]}
       </Graphic>
     </cont>
@@ -138,6 +160,16 @@ function createBitMapFonts(resolution: number) {
       fontFamily: 'Segoe Ui',
       fontSize: 12,
       fill: 0x000000,
+    },
+    { chars: PIXI.BitmapFont.ALPHANUMERIC, resolution }
+  );
+  PIXI.BitmapFont.from(
+    'bold-black',
+    {
+      fontFamily: 'Segoe Ui',
+      fontSize: 12,
+      fill: 0x000000,
+      fontWeight: 'bold',
     },
     { chars: PIXI.BitmapFont.ALPHANUMERIC, resolution }
   );
