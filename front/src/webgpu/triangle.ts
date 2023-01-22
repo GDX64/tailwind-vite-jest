32492abc0b47bc5903115e514a6f6183f459f161
glmachado <glmachado@nelogica.com.br>
2023-01-21 23:10:21 -0300

    refactoring

--- front/src/webgpu/triangle.ts
+++ front/src/webgpu/triangle.ts
@@ -1,65 +1,12 @@
+import { animationFrames } from 'rxjs';
+
 export const webgpuTriangle = async (canvas: HTMLCanvasElement) => {
-  const adapter = await navigator.gpu.requestAdapter();
-  const device = await adapter!.requestDevice();
-
-  const context = canvas.getContext('webgpu') as GPUCanvasContext;
-
-  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
-  context.configure({
-    device,
-    format: presentationFormat,
-    alphaMode: 'opaque',
+  const { device, presentationFormat, context } = await initDevice(canvas);
+  const pipeline = createPipeline(device, presentationFormat);
+  const sub = animationFrames().subscribe(() => {
+    draw(device, context, pipeline);
   });
-
-  const pipeline = device.createRenderPipeline({
-    layout: 'auto',
-    vertex: {
-      module: device.createShaderModule({
-        code: triangleVertWGSL,
-      }),
-      entryPoint: 'main',
-    },
-    fragment: {
-      module: device.createShaderModule({
-        code: redFragWGSL,
-      }),
-      entryPoint: 'main',
-      targets: [
-        {
-          format: presentationFormat,
-        },
-      ],
-    },
-    primitive: {
-      topology: 'triangle-list',
-    },
-  });
-
-  function frame() {
-    const commandEncoder = device.createCommandEncoder();
-    const textureView = context.getCurrentTexture().createView();
-
-    const renderPassDescriptor: GPURenderPassDescriptor = {
-      colorAttachments: [
-        {
-          view: textureView,
-          clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
-          loadOp: 'clear',
-          storeOp: 'store',
-        },
-      ],
-    };
-
-    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-    passEncoder.setPipeline(pipeline);
-    passEncoder.draw(3, 1, 0, 0);
-    passEncoder.end();
-
-    device.queue.submit([commandEncoder.finish()]);
-    requestAnimationFrame(frame);
-  }
-
-  requestAnimationFrame(frame);
+  return () => sub.unsubscribe();
 };
 
 const triangleVertWGSL = /*wgsl*/ `
@@ -83,3 +30,64 @@ fn main() -> @location(0) vec4<f32> {
   return vec4(1.0, 0.0, 0.0, 1.0);
 }
 `;
+
+function draw(device: GPUDevice, context: GPUCanvasContext, pipeline: GPURenderPipeline) {
+  const commandEncoder = device.createCommandEncoder();
+  const textureView = context.getCurrentTexture().createView();
+  const renderPassDescriptor: GPURenderPassDescriptor = {
+    colorAttachments: [
+      {
+        view: textureView,
+        clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+        loadOp: 'clear',
+        storeOp: 'store',
+      },
+    ],
+  };
+  const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+  passEncoder.setPipeline(pipeline);
+  passEncoder.draw(3, 1, 0, 0);
+  passEncoder.end();
+  device.queue.submit([commandEncoder.finish()]);
+}
+
+function createPipeline(device: GPUDevice, presentationFormat: GPUTextureFormat) {
+  return device.createRenderPipeline({
+    layout: 'auto',
+    vertex: {
+      module: device.createShaderModule({
+        code: triangleVertWGSL,
+      }),
+      entryPoint: 'main',
+    },
+    fragment: {
+      module: device.createShaderModule({
+        code: redFragWGSL,
+      }),
+      entryPoint: 'main',
+      targets: [
+        {
+          format: presentationFormat,
+        },
+      ],
+    },
+    primitive: {
+      topology: 'triangle-list',
+    },
+  });
+}
+
+async function initDevice(canvas: HTMLCanvasElement) {
+  const adapter = await navigator.gpu.requestAdapter();
+  const device = await adapter!.requestDevice();
+
+  const context = canvas.getContext('webgpu') as GPUCanvasContext;
+
+  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+  context.configure({
+    device,
+    format: presentationFormat,
+    alphaMode: 'opaque',
+  });
+  return { device, presentationFormat, context };
+}
