05d234c6a018051334b5aa0d05899da6569b4126
gdx <gadu.macahdo@gmail.com>
2023-03-03 14:52:08 -0300

    my page

--- front/src/pages/BackGround.vue
+++ front/src/pages/BackGround.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="w-screen my-container h-screen gradient-color overflow-hidden">
-    <div
-      class="w-full h-full min-h-screen px-3 py-2 text-white flex justify-center overflow-y-auto overflow-x-hidden"
-    >
-      <div class="grow-0 shrink basis-[800px] overflow-x-hidden h-max">
-        <slot></slot>
-      </div>
+  <div class="gradient-color fixed top-0 w-screen h-screen z-[-1]"></div>
+  <div
+    class="w-screen my-container min-h-screen px-3 text-white flex flex-col items-center overflow-y-auto overflow-x-hidden"
+  >
+    <nav class="h-10 w-full border-b-2 border-gray-400 mb-3">
+      <ul class="ml-auto flex items-center h-full">
+        <div class="mr-5">Projects</div>
+        <div>About</div>
+      </ul>
+    </nav>
+    <div class="max-w-[500px] overflow-hidden h-max">
+      <slot></slot>
     </div>
   </div>
 </template>
