a93e1970677b56d1f548c728629f827d280a2b61
glmachado <glmachado@nelogica.com.br>
2023-05-27 21:36:35 -0300

    centering

--- front/src/pages/SimpleRenderer.vue
+++ front/src/pages/SimpleRenderer.vue
@@ -1,85 +1,89 @@
 <template>
-  <div class="w-full px-4 flex flex-col text-base">
-    <h2 class="text-4xl my-2">
-      {{ header }}
-    </h2>
-    <div class="flex flex-col">
-      <div class="whitespace-nowrap mr-2">phase: {{ phase }}</div>
-      <input
-        type="range"
-        class="w-full"
-        :min="0"
-        :max="1"
-        step="0.01"
-        v-model.number="phase"
-      />
-    </div>
-    <div class="flex flex-col mb-2">
-      <div class="w-max whitespace-nowrap mr-2">frequency: {{ frequency }}Hz</div>
-      <input
-        type="range"
-        class="w-full"
-        :min="0"
-        :max="2"
-        step="0.02"
-        v-model.number="frequency"
-      />
-    </div>
-    <GStage class="w-full aspect-auto border border-black h-64">
-      <template #default="{}">
-        <GScale :x-data="scaleData.x" :y-data="scaleData.y">
-          <template #default="{ scaleXY: { x, y, alphaX } }">
-            <PixiSquare
-              v-for="el of elements"
-              :x="x(el - 11)"
-              :y="y(10 * Math.sin((time * frequency * 2 * Math.PI) / 1000 + el * phase))"
-              :height="alphaX"
-              :width="alphaX"
-              fill="#00ffff"
-              stroke="#ff0000"
-            />
-          </template>
-        </GScale>
-      </template>
-    </GStage>
-
-    <p class="py-5">{{ text }}</p>
-
-    <div class="w-max whitespace-nowrap mr-2">balls: {{ balls }}</div>
-    <input
-      type="range"
-      class="w-full"
-      :min="0"
-      :max="16"
-      step="1"
-      v-model.number="balls"
-    />
-    <GStage class="w-full aspect-square border border-black">
-      <template #default>
-        <GScale :x-data="scaleData.x" :y-data="scaleData.y" :no-lines="true">
-          <template #default="{ scaleXY: { x, y, alphaX, alphaY } }">
-            <PIXIEllipse
-              :x="x(0)"
-              :y="y(0)"
-              :height="alphaX * 20"
-              :width="alphaY * 20"
-              stroke="#000000"
-            >
-              <PIXIEllipse
-                v-for="[x, y] of ballsPos"
-                :x="alphaX * x * 10"
-                :y="alphaY * y * 10"
+  <div class="w-full flex flex-col items-center">
+    <div class="w-full px-4 flex flex-col text-base max-w-lg">
+      <h2 class="text-4xl my-2">
+        {{ header }}
+      </h2>
+      <div class="flex flex-col">
+        <div class="whitespace-nowrap mr-2">phase: {{ phase }}</div>
+        <input
+          type="range"
+          class="w-full"
+          :min="0"
+          :max="1"
+          step="0.01"
+          v-model.number="phase"
+        />
+      </div>
+      <div class="flex flex-col mb-2">
+        <div class="w-max whitespace-nowrap mr-2">frequency: {{ frequency }}Hz</div>
+        <input
+          type="range"
+          class="w-full"
+          :min="0"
+          :max="2"
+          step="0.02"
+          v-model.number="frequency"
+        />
+      </div>
+      <GStage class="w-full aspect-[2/1] border border-black">
+        <template #default="{}">
+          <GScale :x-data="scaleData.x" :y-data="scaleData.y">
+            <template #default="{ scaleXY: { x, y, alphaX } }">
+              <PixiSquare
+                v-for="el of elements"
+                :x="x(el - 11)"
+                :y="
+                  y(10 * Math.sin((time * frequency * 2 * Math.PI) / 1000 + el * phase))
+                "
                 :height="alphaX"
-                :width="alphaY"
-                stroke="#000000"
-                fill-style="solid"
-                fill="#2b6083"
+                :width="alphaX"
+                fill="#00ffff"
+                stroke="#ff0000"
               />
-            </PIXIEllipse>
-          </template>
-        </GScale>
-      </template>
-    </GStage>
+            </template>
+          </GScale>
+        </template>
+      </GStage>
+
+      <p class="py-5">{{ text }}</p>
+
+      <div class="w-max whitespace-nowrap mr-2">balls: {{ balls }}</div>
+      <input
+        type="range"
+        class="w-full"
+        :min="0"
+        :max="16"
+        step="1"
+        v-model.number="balls"
+      />
+      <GStage class="w-full aspect-square border border-black">
+        <template #default>
+          <GScale :x-data="scaleData.x" :y-data="scaleData.y" :no-lines="true">
+            <template #default="{ scaleXY: { x, y, alphaX, alphaY } }">
+              <PIXIEllipse
+                :x="x(0)"
+                :y="y(0)"
+                :height="alphaX * 20"
+                :width="alphaY * 20"
+                stroke="#000000"
+              >
+                <PIXIEllipse
+                  v-for="[x, y] of ballsPos"
+                  :x="alphaX * x * 10"
+                  :y="alphaY * y * 10"
+                  :height="alphaX"
+                  :width="alphaY"
+                  stroke="#000000"
+                  fill-style="solid"
+                  fill="#2b6083"
+                />
+              </PIXIEllipse>
+            </template>
+          </GScale>
+        </template>
+      </GStage>
+    </div>
   </div>
 </template>
 
