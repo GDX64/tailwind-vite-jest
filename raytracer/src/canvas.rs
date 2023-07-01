5f2ea9cd649125d2250a5c22135c2906189c61a8
gdx <gadu.macahdo@gmail.com>
2023-07-01 20:24:40 -0300

    canvas

---
+++ raytracer/src/canvas.rs
@@ -0,0 +1,49 @@
+use minifb::{Key, Window, WindowOptions};
+
+pub struct Canvas {
+    width: usize,
+    height: usize,
+    window: Window,
+    pixels: Vec<u32>,
+}
+
+impl Canvas {
+    pub fn new(width: usize, height: usize) -> Canvas {
+        let window = Window::new(
+            "Test - ESC to exit",
+            width,
+            height,
+            WindowOptions::default(),
+        )
+        .unwrap();
+
+        Canvas {
+            width,
+            height,
+            window,
+            pixels: vec![0; width * height],
+        }
+    }
+
+    pub fn clear(&mut self) {
+        for i in self.pixels.iter_mut() {
+            *i = 0;
+        }
+    }
+
+    pub fn write(&mut self, x: usize, y: usize, value: u32) {
+        self.pixels[y * self.width + x] = value;
+    }
+
+    pub fn update_canvas(&mut self) {
+        self.window
+            .update_with_buffer(&self.pixels, self.width, self.height)
+            .unwrap();
+    }
+
+    pub fn loop_until_exit(&mut self) {
+        while self.window.is_open() && !self.window.is_key_down(Key::Escape) {
+            self.update_canvas();
+        }
+    }
+}
--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,3 +1,10 @@
-mod PointVec;
+use minifb::Key;
 
-fn main() {}
+mod PointVec;
+mod canvas;
+
+fn main() {
+    let mut canvas = canvas::Canvas::new(800, 600);
+    canvas.write(200, 200, 0xff0000);
+    canvas.loop_until_exit();
+}
