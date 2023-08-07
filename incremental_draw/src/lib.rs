2c611908d26faeddae8270d23168e77579c83377
gdx <gadu.macahdo@gmail.com>
2023-08-06 23:32:18 -0300

    drawing

--- incremental_draw/src/lib.rs
+++ incremental_draw/src/lib.rs
@@ -1,81 +1,69 @@
-use futures::{stream::StreamExt, Stream};
-use futures_signals::{
-    signal::{Mutable, SignalExt},
-    signal_vec::{MutableVec, SignalVec, SignalVecExt, VecDiff},
-};
+use leptos::log;
+use wasm_bindgen::JsValue;
+use web_sys::CanvasRenderingContext2d;
 
-mod anchors_test;
-
-fn test_signal() -> (impl Stream<Item = f64>, MutableVec<f64>) {
-    let data_points = MutableVec::new();
-
-    let desired_view_slice = Mutable::new((0, 5));
-    let canvas_size = Mutable::new((100, 100));
-
-    let vec_sum =
-        data_points
-            .signal_vec()
-            .to_stream()
-            .scan((0.0, Vec::<f64>::new()), |acc, diff| {
-                let (sum, data_points) = acc;
-                match diff {
-                    VecDiff::Replace { values } => {
-                        *sum = values.iter().clone().sum();
-                        *data_points = values;
-                    }
-                    VecDiff::InsertAt { index, value } => {
-                        data_points.insert(index, value);
-                        *sum += value;
-                    }
-                    VecDiff::UpdateAt { index, value } => {
-                        let el = data_points[index];
-                        data_points[index] = value;
-                        *sum -= el;
-                        *sum += value;
-                    }
-                    VecDiff::RemoveAt { index } => {
-                        let el = data_points.remove(index);
-                        *sum -= el;
-                    }
-                    VecDiff::Clear { .. } => {
-                        *sum = 0.0;
-                        data_points.clear();
-                    }
-                    VecDiff::Move {
-                        old_index,
-                        new_index,
-                    } => {
-                        data_points.swap(old_index, new_index);
-                    }
-                    VecDiff::Pop {} => {
-                        let el = data_points.pop().unwrap();
-                        *sum -= el;
-                    }
-                    VecDiff::Push { value } => {
-                        data_points.push(value);
-                        *sum += value;
-                    }
-                }
-                let sum = *sum;
-                async move { Some(sum) }
-            });
-    return (vec_sum, data_points);
-
-    //block on the stream
+#[derive(PartialEq)]
+pub struct Chart {
+    pub base_data: Vec<f64>,
+    pub view_range: (usize, usize),
+    pub scale_x: LinScale,
+    pub scale_y: LinScale,
+    pub ctx: CanvasRenderingContext2d,
+    pub canvas_size: (u32, u32),
+    pub view_data: Vec<(f64, f64)>,
 }
 
-#[cfg(test)]
-mod tests {
-    use futures::{pin_mut, task::noop_waker};
-    use futures_signals::signal::{from_stream, Signal};
+impl Chart {
+    pub fn recalc(&mut self) {
+        let (min, max) = self.view_range;
+        self.scale_x = LinScale::new((min as f64, max as f64), (0.0, self.canvas_size.0 as f64));
+        let data = &self.base_data[min..max];
+        let (min, max) = data.iter().fold((f64::MAX, f64::MIN), |(min, max), &x| {
+            (min.min(x), max.max(x))
+        });
+        self.scale_y = LinScale::new((min, max), (5.0, self.canvas_size.1 as f64 - 5.0));
+        self.view_data = data
+            .iter()
+            .enumerate()
+            .map(|(i, &x)| (self.scale_x.apply(i as f64), self.scale_y.apply(x)))
+            .collect();
+    }
 
-    use super::*;
+    pub fn draw(&self) {
+        let ctx = &self.ctx;
+        ctx.clear_rect(
+            0.0,
+            0.0,
+            self.canvas_size.0 as f64,
+            self.canvas_size.1 as f64,
+        );
+        ctx.begin_path();
+        if let Some(first_data_point) = self.view_data.first() {
+            ctx.move_to(first_data_point.0, first_data_point.1);
+            for data_point in self.view_data.iter().skip(1) {
+                ctx.line_to(data_point.0, data_point.1);
+            }
+        }
+        let val = JsValue::from_str("red");
+        ctx.set_stroke_style(&val);
+        ctx.stroke();
+    }
+}
 
-    #[tokio::test]
-    async fn it_works() {
-        let (s, v) = test_signal();
-        let mut lock = v.lock_mut();
-        lock.push(3.0);
-        let sig = from_stream(s);
+#[derive(PartialEq)]
+pub struct LinScale {
+    k: f64,
+    alpha: f64,
+}
+
+impl LinScale {
+    pub fn new(domain: (f64, f64), range: (f64, f64)) -> LinScale {
+        let k = (range.1 - range.0) / (domain.1 - domain.0);
+        let alpha = range.0 - k * domain.0;
+        LinScale { k, alpha }
+    }
+
+    pub fn apply(&self, x: f64) -> f64 {
+        self.k * x + self.alpha
     }
 }
--- incremental_draw/src/main.rs
+++ incremental_draw/src/main.rs
@@ -1,4 +1,71 @@
-use leptos::*;
+use incremental_draw::{Chart, LinScale};
+use leptos::{html::*, *};
+use wasm_bindgen::JsCast;
+use web_sys::CanvasRenderingContext2d;
+
 pub fn main() {
-    mount_to_body(|cx| view! { cx,  <p>"Hello, world!"</p> })
+    mount_to_body(|cx| view! { cx,  <MyComponent></MyComponent> })
+}
+
+#[component]
+fn MyComponent(cx: Scope) -> impl IntoView {
+    let canvas_ref: NodeRef<Canvas> = create_node_ref(cx);
+    let chart = create_rw_signal(cx, None as Option<Chart>);
+
+    create_effect(cx, move |_| {
+        if let Some(ctx) = context_from(&canvas_ref) {
+            let mut chart_obj = Chart {
+                base_data: (0..1000).map(|i| ((i as f64) / 100.0).sin()).collect(),
+                view_range: (0, 900),
+                scale_x: LinScale::new((0.0, 5.0), (0.0, 300.0)),
+                scale_y: LinScale::new((0.0, 5.0), (0.0, 100.0)),
+                ctx,
+                canvas_size: (300, 150),
+                view_data: vec![],
+            };
+            chart_obj.recalc();
+            chart.set(Some(chart_obj));
+        }
+    });
+
+    create_effect(cx, move |_| {
+        chart.with(|chart| {
+            chart.as_ref().map(|chart| {
+                chart.draw();
+            });
+        })
+    });
+
+    let advance_chart = move |delta: i32| {
+        chart.update(|chart| {
+            chart.as_mut().map(|chart| {
+                chart.view_range.0 = (delta + chart.view_range.0 as i32) as usize;
+                chart.view_range.1 = (delta + chart.view_range.1 as i32) as usize;
+                chart.recalc();
+            });
+        })
+    };
+
+    view! {
+        cx,
+        <div>
+            <button on:click=move |_| {
+                advance_chart(1);
+            }>+</button>
+            <button on:click=move |_| {
+                advance_chart(-1);
+            }>-</button>
+            <canvas node_ref=canvas_ref></canvas>
+        </div>
+    }
+}
+
+fn context_from(canvas: &NodeRef<Canvas>) -> Option<CanvasRenderingContext2d> {
+    let ctx = canvas
+        .get()?
+        .get_context("2d")
+        .ok()??
+        .dyn_into::<CanvasRenderingContext2d>()
+        .ok()?;
+    Some(ctx)
 }
