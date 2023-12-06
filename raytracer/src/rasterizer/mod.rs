bccfc6e2c6c21c7e30023fb0d45f8203e2c0c9d1
glmachado <glmachado@nelogica.com.br>
2023-12-06 12:49:50 -0300

    simd kind of working

--- raytracer/src/rasterizer/mod.rs
+++ raytracer/src/rasterizer/mod.rs
@@ -2,7 +2,10 @@ use crate::{
     canvas::Canvas,
     point_vec::{TupleLike, V3D},
 };
-use std::simd::{self, cmp::SimdPartialOrd, simd_swizzle};
+use std::{
+    ops::BitAnd,
+    simd::{self, cmp::SimdPartialOrd, simd_swizzle, Mask},
+};
 type Triangle = [V3D; 3];
 trait Boundable {
     fn min_max(&self) -> (V3D, V3D);
@@ -51,9 +54,14 @@ impl TriangleRaster {
         let (min, max) = triangle.as_slice().min_max();
         let simd_triangle = SimdTriangle::from_triangle(triangle);
         for y in min.y as i32..=max.y as i32 {
-            for x in min.x as i32..=max.x as i32 {
-                if simd_triangle.is_inside(x as f32, y as f32) {
-                    f(x, y);
+            //loop x 4 by 4
+            for x in (min.x as i32..=max.x as i32).step_by(4) {
+                let x = x as f32;
+                let vx = simd::f32x4::from_array([x, x + 1.0, x + 2.0, x + 3.0]);
+                let vy = simd::f32x4::splat(y as f32);
+                let mask = simd_triangle.is_inside(vx, vy);
+                if mask.all() {
+                    f(x as i32, y);
                 }
             }
         }
@@ -72,48 +80,60 @@ impl TriangleRaster {
 }
 
 struct SimdTriangle {
-    x: simd::f32x4,
-    y: simd::f32x4,
-    vx: simd::f32x4,
-    vy: simd::f32x4,
-    mask: simd::f32x4,
+    x1: simd::f32x4,
+    x2: simd::f32x4,
+    x3: simd::f32x4,
+    y1: simd::f32x4,
+    y2: simd::f32x4,
+    y3: simd::f32x4,
+    vx1: simd::f32x4,
+    vx2: simd::f32x4,
+    vx3: simd::f32x4,
+    vy1: simd::f32x4,
+    vy2: simd::f32x4,
+    vy3: simd::f32x4,
 }
 
 impl SimdTriangle {
     fn from_triangle(triangle: &Triangle) -> SimdTriangle {
-        let x = simd::f32x4::from_array([
-            triangle[0].x as f32,
-            triangle[1].x as f32,
-            triangle[2].x as f32,
-            0.0,
-        ]);
-        let y = simd::f32x4::from_array([
-            triangle[0].y as f32,
-            triangle[1].y as f32,
-            triangle[2].y as f32,
-            0.0,
-        ]);
-        let x_shifted = simd_swizzle!(x, [1, 2, 0, 3]);
-        let y_shifted = simd_swizzle!(y, [1, 2, 0, 3]);
-        let vx = x_shifted - x;
-        let vy = y_shifted - y;
+        let (x1, x2, x3) = (
+            simd::f32x4::splat(triangle[0].x as f32),
+            simd::f32x4::splat(triangle[1].x as f32),
+            simd::f32x4::splat(triangle[2].x as f32),
+        );
+        let (y1, y2, y3) = (
+            simd::f32x4::splat(triangle[0].y as f32),
+            simd::f32x4::splat(triangle[1].y as f32),
+            simd::f32x4::splat(triangle[2].y as f32),
+        );
+        let (vx1, vx2, vx3) = (x2 - x1, x3 - x2, x1 - x3);
+        let (vy1, vy2, vy3) = (y2 - y1, y3 - y2, y1 - y3);
         SimdTriangle {
-            x,
-            y,
-            vx,
-            vy,
-            mask: simd::f32x4::splat(0.0),
+            x1,
+            x2,
+            x3,
+            y1,
+            y2,
+            y3,
+            vx1,
+            vx2,
+            vx3,
+            vy1,
+            vy2,
+            vy3,
         }
     }
 
-    fn is_inside(&self, x: f32, y: f32) -> bool {
-        let px = simd::f32x4::splat(x);
-        let py = simd::f32x4::splat(y);
-        let pv1 = self.x - px;
-        let pv2 = self.y - py;
-        let cross_z = self.vx * pv2 - self.vy * pv1;
-        let mask = cross_z.simd_ge(self.mask);
-        mask.all()
+    fn is_inside(&self, x: simd::f32x4, y: simd::f32x4) -> simd::Mask<i32, 4> {
+        let zeros = simd::f32x4::splat(0.0);
+        let cross0 = (x - self.x1) * self.vy1 - (y - self.y1) * self.vx1;
+        let cross1 = (x - self.x2) * self.vy2 - (y - self.y2) * self.vx2;
+        let cross2 = (x - self.x3) * self.vy3 - (y - self.y3) * self.vx3;
+        let sign0 = cross0.simd_ge(zeros);
+        let sign1 = cross1.simd_ge(zeros);
+        let sign2 = cross2.simd_ge(zeros);
+        let mask = sign0 & sign1 & sign2;
+        mask
     }
 }
 
@@ -136,14 +156,5 @@ mod test {
     }
 
     #[test]
-    fn simd_inside() {
-        let triangle = [
-            super::V3D::new(150.0, 100.0, 0.0),
-            super::V3D::new(100.0, 200.0, 0.0),
-            super::V3D::new(200.0, 200.0, 0.0),
-        ];
-        let simd_triangle = super::SimdTriangle::from_triangle(&triangle);
-        assert_eq!(simd_triangle.is_inside(150.0, 150.0), true);
-        // assert_eq!(simd_triangle.is_inside(60.0, 50.0), false);
-    }
+    fn simd_inside() {}
 }
