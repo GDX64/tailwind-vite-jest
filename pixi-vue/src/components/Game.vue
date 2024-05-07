e6e2170e7e5d1bd8d8b386b96e6473206d7cf86a
gdx <gadu.macahdo@gmail.com>
2024-05-06 22:52:59 -0300

    updates

--- pixi-vue/src/components/Game.vue
+++ pixi-vue/src/components/Game.vue
@@ -1,17 +1,18 @@
 <template>
   <g-container>
     <g-sprite
-      :x="position.x"
-      :y="position.y"
-      :rotation="rotation"
+      v-for="boid in boids"
+      :x="boid.position.x"
+      :y="boid.position.y"
+      :rotation="boid.rotation"
       :texture="texture"
-      :scale="0.1"
+      :scale="0.03"
     ></g-sprite>
   </g-container>
 </template>
 
 <script setup lang="ts">
-import { shallowRef, ref } from "vue";
+import { shallowRef, triggerRef } from "vue";
 import {
   usePixiAnimation,
   usePixiAppData,
@@ -20,6 +21,8 @@ import {
 import { Assets, Point, Texture } from "pixi.js";
 import baseSpritePath from "../assets/neon.png";
 import { reactive, onUnmounted } from "vue";
+import { Vec2 } from "../utils/Vec2";
+
 const SCENE_WIDTH = 50;
 
 const texture = shallowRef<Texture>();
@@ -28,13 +31,39 @@ Assets.load([baseSpritePath]).then(() => {
 });
 
 class Boid {
-  position;
+  position = new Vec2(0, 0);
+  velocity = new Vec2(0, 0);
+
+  get rotation() {
+    return Math.atan2(this.velocity.y, this.velocity.x);
+  }
+
+  isNear(other: Boid) {
+    return this.position.sub(other.position).length() < 10;
+  }
 }
 
-const position = reactive(new Point(0, 0));
-const rotation = ref(0);
+const boids = shallowRef<Boid[]>([]);
+
+boids.value = Array.from({ length: 100 }, () => {
+  const boid = new Boid();
+  boid.position.x = Math.random() * SCENE_WIDTH - SCENE_WIDTH / 2;
+  boid.position.y = Math.random() * SCENE_WIDTH - SCENE_WIDTH / 2;
+  boid.velocity.set(Math.random() * 2 - 1, Math.random() * 2 - 1);
+  return boid;
+});
 
 const data = usePixiAppData();
+
+usePixiAnimation(() => {
+  const dt = 0.016;
+  boids.value.forEach((boid) => {
+    boid.position = boid.position.add(boid.velocity.scale(dt));
+    boid.position.x = boid.position.x % SCENE_WIDTH;
+    boid.position.y = boid.position.y % SCENE_WIDTH;
+  });
+  triggerRef(boids);
+});
 
 const app = usePixiApp();
 app.stage.position.set(data.width / 2, data.height / 2);
---
+++ pixi-vue/src/utils/Vec2.ts
@@ -0,0 +1,33 @@
+export class Vec2 {
+  constructor(public x: number, public y: number) {}
+
+  set(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(v: Vec2) {
+    return new Vec2(this.x + v.x, this.y + v.y);
+  }
+
+  sub(v: Vec2) {
+    return new Vec2(this.x - v.x, this.y - v.y);
+  }
+
+  scale(k: number) {
+    return new Vec2(this.x * k, this.y * k);
+  }
+
+  dot(v: Vec2) {
+    return this.x * v.x + this.y * v.y;
+  }
+
+  length() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
+  normalize() {
+    const l = this.length();
+    return new Vec2(this.x / l, this.y / l);
+  }
+}
