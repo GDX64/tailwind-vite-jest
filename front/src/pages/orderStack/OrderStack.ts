738a3b6c61b524793762f19ba34c9b85db967f26
glmachado <glmachado@nelogica.com.br>
2024-08-15 22:11:05 -0300

    priority working

--- front/src/pages/orderStack/OrderStack.ts
+++ front/src/pages/orderStack/OrderStack.ts
@@ -1,5 +1,6 @@
 export type StackObject = {
   size: number;
+  priority: number;
   position: number;
 };
 
@@ -31,9 +32,21 @@ export class OrderStack<T extends StackObject> {
   }
 
   private _run(arr: T[]) {
-    let groups = arr
-      .map((obj) => Group.from([obj], this.upperLimit, this.lowerLimit))
-      .sort((a, b) => a.position - b.position);
+    if (arr.length === 0) return { newGroups: [], iterations: 0 };
+
+    const lowestPriority = arr.reduce(
+      (acc, obj) => Math.min(acc, obj.priority),
+      Infinity
+    );
+    const higherPriorityObjects = arr.filter((obj) => obj.priority > lowestPriority);
+    const highResult = this._run(higherPriorityObjects)?.newGroups ?? [];
+    const myGroup = arr.filter((obj) => obj.priority === lowestPriority);
+
+    let groups = myGroup.map((obj) =>
+      Group.from([obj], this.upperLimit, this.lowerLimit)
+    );
+
+    groups = [...highResult, ...groups].sort((a, b) => a.position - b.position);
 
     const MAX_ITERATIONS = groups.length + 1;
 
@@ -48,7 +61,15 @@ export class OrderStack<T extends StackObject> {
         }
         if (currentNewGroup.overlaps(groups[i])) {
           overlap = true;
-          currentNewGroup.merge(groups[i]);
+          if (currentNewGroup.areSamePriority(groups[i])) {
+            currentNewGroup.merge(groups[i]);
+          } else if (currentNewGroup.priority > groups[i].priority) {
+            groups[i].setLowerLimit(currentNewGroup.position + currentNewGroup.size);
+            newGroups.push(groups[i]);
+          } else {
+            currentNewGroup.setUpperLimit(groups[i].position);
+            newGroups.push(groups[i]);
+          }
         } else {
           newGroups.push(groups[i]);
         }
@@ -58,7 +79,7 @@ export class OrderStack<T extends StackObject> {
       }
       groups = newGroups;
     }
-    return null;
+    return { newGroups: groups, iterations: MAX_ITERATIONS };
   }
 }
 
@@ -66,8 +87,23 @@ class Group<T extends StackObject> implements StackObject {
   objects: T[] = [];
   size = 0;
   position = 0;
+  priority = 0;
 
   constructor(private uppperLimit: number, private lowerLimit: number) {}
+
+  setLowerLimit(limit: number) {
+    this.lowerLimit = limit;
+    this.position = clamp(this.position, this.lowerLimit, this.uppperLimit - this.size);
+  }
+
+  setUpperLimit(limit: number) {
+    this.uppperLimit = limit;
+    this.position = clamp(this.position, this.lowerLimit, this.uppperLimit - this.size);
+  }
+
+  areSamePriority(other: Group<T>) {
+    return this.priority === other.priority;
+  }
 
   overlaps(other?: Group<T>) {
     if (!other) return false;
@@ -92,6 +128,7 @@ class Group<T extends StackObject> implements StackObject {
     group.objects = objects;
     group.size = width;
     group.position = x;
+    group.priority = objects[0].priority ?? 0;
     return group;
   }
 
--- front/src/pages/orderStack/OrderStack.vue
+++ front/src/pages/orderStack/OrderStack.vue
@@ -1,6 +1,10 @@
 <template>
   <BackGround>
     <div class="py-4">Iterations {{ stacked?.iterations }}</div>
+    <div class="flex items-center gap-4">
+      <div class="">Drag with priority</div>
+      <input type="checkbox" v-model="dragWithPriority" class="h-5 w-5"></input>
+    </div>
     <div
       class="w-full bg-sec-900 flex flex-col relative select-none mt-5 touch-none"
       :style="{ height: height + 'px' }"
@@ -8,22 +12,22 @@
       @pointermove="onPointerMove"
     >
       <div
-        class="absolute bg-prime-600 w-10 border border-white left-5"
+        class="absolute bg-prime-600 w-28 border border-white left-5"
         :class="dragging ? 'pointer-events-none' : 'pointer-events-auto'"
-        v-for="obj of stacked?.objects ?? []"
-        @pointerdown="onPointerDown(obj.original)"
-        :style="{ height: obj.original.size + 'px', top: obj.x + 'px' }"
+        v-for="obj of objects"
+        @pointerdown="onPointerDown(obj)"
+        :style="{ height: obj.size + 'px', top: obj.showPosition + 'px' }"
       >
-        {{ obj.original.id }}
+        {{ obj.id }}
       </div>
       <div
-        v-for="obj of stacked?.objects ?? []"
-        class="absolute left-16 flex items-center -translate-y-1/2 gap-2"
-        :style="{ top: obj.original.position + obj.original.size / 2 + 'px' }"
+        v-for="obj of objects"
+        class="absolute left-28 flex items-center -translate-y-1/2 gap-2 pointer-events-none"
+        :style="{ top: obj.position + obj.size / 2 + 'px' }"
       >
         <div class="w-56 h-[1px] bg-white"></div>
         <div class="">
-          {{ obj.original.id }}
+          {{ obj.id }}
         </div>
       </div>
     </div>
@@ -34,15 +38,22 @@
 import { computed, ref } from 'vue';
 import { OrderStack, StackObject } from './OrderStack';
 import BackGround from '../BackGround.vue';
+import { useAnimationFrames } from '../../utils/rxjsUtils';
 
-const dragging = ref<StackObject | null>(null);
+type DragObj = StackObject & { id: number; priority: number };
+const dragging = ref<DragObj | null>(null);
+const dragWithPriority = ref(false);
 
-function onPointerDown(obj: StackObject) {
+function onPointerDown(obj: DragObj) {
   dragging.value = obj;
+  dragging.value.priority = dragWithPriority.value ? 1 : 0;
 }
 
 function onPointerUp() {
-  dragging.value = null;
+  if (dragging.value) {
+    dragging.value.priority = 0;
+    dragging.value = null;
+  }
 }
 
 function onPointerMove(event: PointerEvent) {
@@ -52,23 +63,46 @@ function onPointerMove(event: PointerEvent) {
 }
 
 const objects = ref([
-  ...Array(5)
+  ...Array(8)
     .fill(0)
     .map((_, i) => {
       return {
         id: i,
         position: i * 60 + 100,
-        size: 50 + i * 10,
+        showPosition: i * 60 + 100,
+        size: 40 ,
+        priority: 0,
       };
     }),
 ]);
+
+useAnimationFrames(() => {
+  objects.value.forEach((obj, i) => {
+    const findWithId = stacked.value?.objects.find((o) => o.original.id === obj.id);
+    if (findWithId) {
+      const isBeingDragged = dragging.value?.id === obj.id;
+      if (isBeingDragged) {
+        obj.showPosition = findWithId.x;
+      } else {
+        const diff = findWithId.x - obj.showPosition;
+        obj.showPosition = obj.showPosition + diff / 5;
+      }
+    }
+  });
+});
+
 const height = ref(600);
 
 const stacked = computed(() => {
   return stack.run(objects.value);
 });
 
-const stack = new OrderStack<{ id: number; position: number; size: number }>();
+const stack = new OrderStack<{
+  id: number;
+  position: number;
+  size: number;
+  priority: number;
+}>();
 stack.lowerLimit = 0;
 stack.upperLimit = height.value;
 </script>
