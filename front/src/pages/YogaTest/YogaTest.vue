bd1f081f0e905a27579e3f162315a3bd77297b2f
glmachado <glmachado@nelogica.com.br>
2024-08-27 17:13:16 -0300

    trying a bit of abstraction

--- front/src/pages/YogaTest/YogaTest.vue
+++ front/src/pages/YogaTest/YogaTest.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="w-screen h-screen flex flex-col">
-    <input type="range" class="w-96" v-model.number="b1Width" min="100" max="500"></input>
-    <input type="range" class="w-96" v-model.number="b2Height" min="100" max="500"></input>
-    <input type="range" class="w-96" v-model.number="b1Height" min="100" max="500"></input>
     <canvas class="grow" ref="canvas"> </canvas>
   </div>
 </template>
@@ -10,47 +7,72 @@
 <script lang="ts" setup>
 import { ref, watchEffect } from 'vue';
 import { useAnimationFrames, useCanvasDPI } from '../../utils/rxjsUtils';
-import Yoga from 'yoga-layout';
+import Yoga, { Node } from 'yoga-layout';
 
-const root = Yoga.Node.create();
+const measureCtx = new OffscreenCanvas(0, 0).getContext('2d')!;
 
-const b1Width = ref(100);
-const b1Height = ref(100);
-const b2Height = ref(100);
+class BoxEl {
+  layout: Node = Yoga.Node.create();
+  children: BoxEl[] = [];
+  render?: (ctx: CanvasRenderingContext2D) => void = undefined;
+  constructor() {}
 
-const b1 = Yoga.Node.create();
-b1.setWidth(100);
-b1.setHeight(100);
-b1.setAlignItems(Yoga.ALIGN_CENTER);
-b1.setFlexDirection(Yoga.FLEX_DIRECTION_ROW);
-b1.setJustifyContent(Yoga.JUSTIFY_SPACE_AROUND);
+  insertChildren(box: BoxEl) {
+    this.children.push(box);
+    this.layout.insertChild(box.layout, this.children.length - 1);
+  }
 
-const b1Child = Yoga.Node.create();
-b1.insertChild(b1Child, 0);
-b1Child.setWidth(50);
-b1Child.setHeight(50);
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.save();
+    ctx.translate(this.layout.getComputedLeft(), this.layout.getComputedTop());
+    this.render?.(ctx);
+    this.children.forEach((child) => child.draw(ctx));
+    ctx.restore();
+  }
+}
 
-const b1Child2 = Yoga.Node.create();
-b1.insertChild(b1Child2, 0);
-b1Child2.setWidth(20);
-b1Child2.setHeight(50);
+const root = new BoxEl();
 
-const b2 = Yoga.Node.create();
-b2.setWidth(100);
-b2.setHeight(100);
+const b1 = new BoxEl();
+b1.render = (ctx) => {
+  ctx.fillStyle = '#ff0000';
+  ctx.fillRect(0, 0, b1.layout.getComputedWidth(), b1.layout.getComputedHeight());
+};
+b1.layout.setWidth(200);
+b1.layout.setHeight(200);
+b1.layout.setAlignItems(Yoga.ALIGN_CENTER);
+b1.layout.setJustifyContent(Yoga.JUSTIFY_CENTER);
 
+const b1Text = new BoxEl();
+b1Text.render = (ctx) => {
+  ctx.fillStyle = '#ffffff';
+  ctx.font = '20px Arial';
+  ctx.fillText('Hello World', 0, 20);
+};
 
-root.insertChild(b2, 0);
-root.insertChild(b1, 1);
+measureCtx.font = '20px Arial';
+const b1TextWidth = measureCtx.measureText('Hello World').width;
+b1Text.layout.setWidth(b1TextWidth);
+b1Text.layout.setHeight(25);
+b1.insertChildren(b1Text);
+
+const b2 = new BoxEl();
+b2.render = (ctx) => {
+  ctx.fillStyle = '#ffff00';
+  ctx.fillRect(0, 0, b2.layout.getComputedWidth(), b2.layout.getComputedHeight());
+};
+
+b2.layout.setWidth(200);
+b2.layout.setHeight(200);
+
+root.insertChildren(b1);
+root.insertChildren(b2);
 
 const { canvas, pixelSize, size } = useCanvasDPI();
 
-watchEffect(()=>{
-  b1.setWidth(b1Width.value);
-  b1.setHeight(b1Height.value);
-  b2.setHeight(b2Height.value);
-  root.calculateLayout(size.width, size.height);
-})
+watchEffect(() => {
+  root.layout.calculateLayout(size.width, size.height);
+});
 
 useAnimationFrames(() => {
   const ctx = canvas.value?.getContext('2d');
@@ -59,34 +81,7 @@ useAnimationFrames(() => {
   ctx.save();
   ctx.clearRect(0, 0, size.width, size.height);
   ctx.scale(devicePixelRatio, devicePixelRatio);
-  ctx.fillStyle = 'red';
-
-  ctx.save();
-  ctx.translate(b1.getComputedLeft(), b1.getComputedTop());
-  ctx.fillRect(0, 0, b1.getComputedWidth(), b1.getComputedHeight());
-
-  ctx.save();
-  ctx.translate(b1Child.getComputedLeft(), b1Child.getComputedTop());
-  ctx.fillStyle = 'yellow';
-  ctx.fillRect(0, 0, b1Child.getComputedWidth(), b1Child.getComputedHeight());
-  ctx.restore();
-
-  ctx.save();
-  ctx.translate(b1Child2.getComputedLeft(), b1Child2.getComputedTop());
-  ctx.fillStyle = 'blue';
-  ctx.fillRect(0, 0, b1Child2.getComputedWidth(), b1Child2.getComputedHeight());
-  ctx.restore();
-
-  ctx.restore();
-
-  ctx.fillStyle = 'green';
-  ctx.fillRect(
-    b2.getComputedLeft(),
-    b2.getComputedTop(),
-    b2.getComputedWidth(),
-    b2.getComputedHeight()
-  );
-
+  root.draw(ctx);
   ctx.restore();
 });
 </script>
