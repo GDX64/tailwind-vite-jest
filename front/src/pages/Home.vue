fc96ec3d8d8ecf37bc21f08c192e465fa86b873f
glmachado <glmachado@nelogica.com.br>
2024-10-13 00:58:46 -0300

    changed link

--- front/src/pages/Home.vue
+++ front/src/pages/Home.vue
@@ -7,9 +7,7 @@ import BackGround from './BackGround.vue';
     <h2 class="text-xl mb-4">The archpelagus saga</h2>
     <ul class="flex flex-col gap-4 text-xl [&_a]:text-high-300 [&_a]:underline">
       <li>
-        <a href="https://archpelagus.glmachado.com/static/editor/index.html"
-          >GAME - Archpelagus 🚢</a
-        >
+        <a href="https://archpelagus.glmachado.com/static/">GAME - Archpelagus 🚢</a>
         <p>Online Real Time Strategy game (kind of age of empires, but with boats)</p>
       </li>
       <li>
