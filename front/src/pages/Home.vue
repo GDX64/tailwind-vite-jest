5fc3632ab1e9845bdefd07a38f0214bb25f54859
glmachado <glmachado@nelogica.com.br>
2024-11-09 21:14:50 -0300

    adjusting sizes

--- front/src/pages/Home.vue
+++ front/src/pages/Home.vue
@@ -12,22 +12,30 @@ import NavBar from '../components/NavBar.vue';
   <NavBar />
   <div class="flex flex-col items-center px-10 text-text-prime">
     <div class="max-w-[1420px] w-full">
-      <div class="py-72 w-full flex justify-between">
-        <div class="">
-          <label for="" class="text-text-label text-2xl font-semibold">I am</label>
-          <h1 class="text-6xl leading-[1.8] font-extrabold">Gabriel Machado</h1>
-          <h1 class="text-6xl font-extrabold">Cross-platform Developer</h1>
+      <section class="w-full min-h-[calc(100vh-96px)] flex flex-col">
+        <div class="grow flex flex-col justify-center py-10">
+          <div class="w-full flex justify-between">
+            <div class="">
+              <label for="" class="text-text-label text-2xl font-semibold">I am</label>
+              <h1 class="text-6xl leading-[1.8] font-extrabold">Gabriel Machado</h1>
+              <h1 class="text-6xl font-extrabold">Cross-platform Developer</h1>
+            </div>
+            <div class="flex flex-col justify-between">
+              <Linkedin
+                class="hover:text-hover hover:scale-105 transition-all"
+              ></Linkedin>
+              <Github class="hover:text-hover hover:scale-105 transition-all"></Github>
+              <Instagram
+                class="hover:text-hover hover:scale-105 transition-all"
+              ></Instagram>
+            </div>
+          </div>
         </div>
-        <div class="flex flex-col justify-between">
-          <Linkedin class="hover:text-hover hover:scale-105 transition-all"></Linkedin>
-          <Github class="hover:text-hover hover:scale-105 transition-all"></Github>
-          <Instagram class="hover:text-hover hover:scale-105 transition-all"></Instagram>
-        </div>
-      </div>
 
-      <div class="flex justify-center w-full">
-        <Button class="flex items-center gap-2"><span>About Me</span> <ADown /></Button>
-      </div>
+        <div class="flex justify-center w-full py-10">
+          <Button class="flex items-center gap-2"><span>About Me</span> <ADown /></Button>
+        </div>
+      </section>
     </div>
   </div>
 </template>
