9e3e526c2243dd188bc1265589118257f83c86fc
glmachado <glmachado@nelogica.com.br>
2024-11-16 01:29:10 -0300

    added animations

--- front/src/components/Experience.vue
+++ front/src/components/Experience.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="flex flex-col gap-3 border-l-2 border-text-label pl-3">
+  <div
+    class="w-full flex flex-col gap-3 border-l-2 border-text-label hover:border-prime-500 pl-3 cursor-pointer hover:bg-prime-100 pr-2 py-2 rounded-md hover:translate-x-1 transition-all"
+    @click="isOpen = !isOpen"
+  >
     <div class="w-full flex justify-between text-lg">
       <h3 class="font-bold">{{ title }}</h3>
       <h3 class="underline text-prime-900">{{ company }}</h3>
     </div>
     <div class="text-text-label font-semibold">{{ date }}</div>
+    <div class="transition-all w-full overflow-hidden" :class="isOpen ? 'h-24' : 'h-0'">
+      {{ description }}
+    </div>
     <div class="flex gap-2">
       <TagList :tags="tags" />
     </div>
@@ -12,11 +18,16 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import TagList from './TagList.vue';
+
+const isOpen = ref(false);
+
 defineProps<{
   title: string;
   company: string;
   date: string;
   tags: string[];
+  description: string;
 }>();
 </script>
--- front/src/pages/Home.vue
+++ front/src/pages/Home.vue
@@ -48,8 +48,56 @@ import InstagramRound from '../components/Contacts/InstagramRound.vue';
         </a>
       </div>
     </section>
-    <section id="about-me" class="w-full pt-20 snap-start">
+    <section id="about-me" class="w-full pt-20">
       <div class="flex justify-between flex-wrap gap-4">
+        <div class="max-w-[600px] grow">
+          <h3 class="text-xl text-text-label font-semibold pb-4">Experiences</h3>
+          <div class="flex flex-col gap-4">
+            <Experience
+              company="Nelogica"
+              date="2024 - Now"
+              description="In late 2024 I became tech lead of the Profit Web team to focus again on the Profit Web project as well as web front end libraries and APIs."
+              title="Tech Lead"
+              :tags="['Web', 'FrontEnd']"
+            ></Experience>
+            <Experience
+              company="Nelogica"
+              date="2023 - 2024"
+              title="Software Developer IV"
+              description="In 2023 I moved to Porto Alegre to work in office with the mobile team using our typescript codebase to target the mobile platforms. Today we have hundreds of thousands active users of the Profit Android and IOS apps monthly."
+              :tags="['iOS', 'Android', 'CapacitorJS']"
+            ></Experience>
+            <Experience
+              company="Nelogica"
+              date="2022 - 2023"
+              title="Software Developer III"
+              description="In late 2022 we developed the Profit Mac project using web technologies and electron, briging our products to thousands of MacOS users."
+              :tags="['Electron', 'MacOS']"
+            ></Experience>
+            <Experience
+              company="Nelogica"
+              date="2021 - 2022"
+              title="Software Developer II"
+              description="As I acquired more skills and experience, I started to guide my work towards the aplication performance and developer tools.
+              In this year I introduced typescript to the web projects."
+              :tags="['Typescript', 'VueJS']"
+            ></Experience>
+            <Experience
+              company="Nelogica"
+              date="2020 - 2021"
+              title="Software Developer I"
+              description="In my first year at Nelogica, I worked on the Homebroker and ProfitWeb projects, using vuejs. The homebroker is a trading platform used by thousands of traders every day in several brazilian exchanges."
+              :tags="['Javascript', 'HTML', 'CSS']"
+            ></Experience>
+            <Experience
+              company="PSA (Peugeot Citroën)"
+              date="2019"
+              title="Engeenier Intern"
+              description="I worked in the maintenance department, where I helped with spare parts management and maintenance planning."
+              :tags="['Excel']"
+            ></Experience>
+          </div>
+        </div>
         <div class="max-w-[560px] min-w-[380px] flex flex-col gap-7 text-xl">
           <p>
             Electrical Engineer turned Software Developer with a specialization in
@@ -66,47 +114,6 @@ import InstagramRound from '../components/Contacts/InstagramRound.vue';
             across platforms.
           </p>
         </div>
-        <div class="w-[400px]">
-          <h3 class="text-xl text-text-label font-semibold pb-4">Experiences</h3>
-          <div class="flex flex-col gap-8">
-            <Experience
-              company="Nelogica"
-              date="2024 - Now"
-              title="Tech Lead"
-              :tags="['Web', 'FrontEnd']"
-            ></Experience>
-            <Experience
-              company="Nelogica"
-              date="2023 - 2024"
-              title="Software Developer IV"
-              :tags="['iOS', 'Android', 'CapacitorJS']"
-            ></Experience>
-            <Experience
-              company="Nelogica"
-              date="2022 - 2023"
-              title="Software Developer III"
-              :tags="['Electron', 'MacOS']"
-            ></Experience>
-            <Experience
-              company="Nelogica"
-              date="2021 - 2022"
-              title="Software Developer II"
-              :tags="['Typescript', 'Webpack', 'WebPlatform']"
-            ></Experience>
-            <Experience
-              company="Nelogica"
-              date="2020 - 2021"
-              title="Software Developer I"
-              :tags="['Javascript', 'HTML', 'CSS']"
-            ></Experience>
-            <Experience
-              company="PSA (Peugeot Citroën)"
-              date="2019"
-              title="Engeenier Intern"
-              :tags="['Excel']"
-            ></Experience>
-          </div>
-        </div>
       </div>
       <div class="flex justify-center w-full pt-10">
         <a href="#projects">
@@ -114,7 +121,7 @@ import InstagramRound from '../components/Contacts/InstagramRound.vue';
         </a>
       </div>
     </section>
-    <section id="projects" class="py-20 snap-start">
+    <section id="projects" class="py-20">
       <div class="w-full pb-10">
         <label class="text-text-label text-lg font-bold">Personal projects</label>
         <h2 class="font-bold text-5xl mt-5">What I've been working on</h2>
